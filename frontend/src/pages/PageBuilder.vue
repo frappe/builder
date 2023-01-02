b91f1bb22aa9bc9e84e80b0bd7f85a2439c8cf94
Suraj Shetty <surajshetty3416@gmail.com>
2023-01-02 17:00:28 +0530

    WIP

---
+++ frontend/src/pages/PageBuilder.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<div class="widgets flex">Hello</div>
+		<div class="canvas flex">This</div>
+		<div class="properties">Hello</div>
+	</div>
+</template>
+
+<script setup>
+</script>
+<style>
+</style>
