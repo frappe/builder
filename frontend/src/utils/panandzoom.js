88039ba53e9cb2db3d4fa5cbcd46619b69909109
Suraj Shetty <surajshetty3416@gmail.com>
2023-01-16 10:59:52 +0530

    fix: Pan and Zoom
    - Used only "wheel" event for panning and zooming

--- frontend/src/utils/panandzoom.js
+++ frontend/src/utils/panandzoom.js
@@ -2,28 +2,30 @@ export { set_pan_and_zoom }
 
 function set_pan_and_zoom(element, zoom_limits = { min: 0.5, max: 2 }) {
 	if (element.parentElement) {
-		element.parentElement.addEventListener('mousemove', function (e) {
-			if (e.buttons === 1) {
-				let x = (element.previousX || 0) + e.movementX
-				let y = (element.previousY || 0) + e.movementY
+		element.parentElement.addEventListener('wheel', function (e) {
+			e.preventDefault()
+			let scale = (element.previousScale || 1)
+			let x = (element.previousX || 0)
+			let y = (element.previousY || 0)
+
+			if (e.ctrlKey) {
+				scale -= e.deltaY * 0.01;
+				if (scale < zoom_limits.min) scale = zoom_limits.min
+				if (scale > zoom_limits.max) scale = zoom_limits.max
+				element.style.transform = `scale(${scale}) translate(${
+					element.previousX || 0
+				}px, ${element.previousY || 0}px)`
+				element.previousScale = scale
+			} else {
+				x -= e.deltaX * 2;
+				y -= e.deltaY * 2;
 				element.style.transform = `translate(${x}px, ${y}px) scale(${
 					element.previousScale || 1
 				})`
 				element.previousX = x
 				element.previousY = y
 			}
-		})
-
-		element.parentElement.addEventListener('wheel', function (e) {
-			e.preventDefault()
-			let scale = (element.previousScale || 1) + e.deltaY * -0.01
-			if (scale < zoom_limits.min) scale = zoom_limits.min
-			if (scale > zoom_limits.max) scale = zoom_limits.max
-			element.style.transform = `scale(${scale}) translate(${
-				element.previousX || 0
-			}px, ${element.previousY || 0}px)`
-			element.previousScale = scale
-		})
+		}, { passive: false })
 
 		function set_reset_event(element) {
 			element.addEventListener('dblclick', function (e) {
