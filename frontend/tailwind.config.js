9f68393c97e9d402d745a919770d541700391aa1
Suraj Shetty <surajshetty3416@gmail.com>
2023-01-22 21:02:01 +0530

    style: Fix Formatting

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,16 @@
 module.exports = {
-  presets: [
-    require('frappe-ui/src/utils/tailwind.config')
-  ],
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	presets: [require("frappe-ui/src/utils/tailwind.config")],
+	content: [
+		"./index.html",
+		"./src/**/*.{vue,js,ts,jsx,tsx}",
+		"./node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			transitionProperty: {
+				size: "transform, border-radius",
+			},
+		},
+	},
+	plugins: [],
+};
