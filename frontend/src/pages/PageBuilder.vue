9f3a58b6aa3da3549c0e70ec3bf08ced1b238692
Suraj Shetty <surajshetty3416@gmail.com>
2023-01-30 22:05:36 +0530

    chore: Remove unwanted code

--- frontend/src/pages/PageBuilder.vue
+++ frontend/src/pages/PageBuilder.vue
@@ -14,17 +14,4 @@ import BuilderCanvas from "../components/BuilderCanvas.vue";
 import BuilderToolbar from "../components/BuilderToolbar.vue";
 import BuilderSidebar from "../components/BuilderSidebar.vue";
 import BlockProperties from "../components/BlockProperties.vue";
-
-// const webpages = createResource({
-// 	url: "frappe.client.get_list",
-// 	onSuccess(data) {
-// 		console.log(data);
-// 	}
-// });
-
-// webpages.submit({
-// 	doctype: "Web Page Beta",
-// 	fields: ["name", "title", "route", "options"],
-// });
-
 </script>
