3ee5099a060fcef0c66f46d7824c86e7584587f9
Suraj Shetty <surajshetty3416@gmail.com>
2023-01-31 14:07:38 +0530

    feat: Border radius updater

--- frontend/src/components/BlockEditor.vue
+++ frontend/src/components/BlockEditor.vue
@@ -1,14 +1,6 @@
 <template>
-	<div class="z-10 editor fixed hover:border-[1px] hover:border-blue-200 group" ref="editor" @click.stop="handleClick">
-		<div class="absolute top-[-32px] w-auto h-auto pointer-events-auto">
-			<Dropdown
-				:options="[{label: 'H1', value: 'h1', handler: () => setType('h1')}, {label: 'H2', value: 'h2', handler: () => setType('h1')}]"
-				:button="{ label: 'Font'}">
-			</Dropdown>
-		</div>
-		<div class="absolute w-[8px] h-[8px] bg-white rounded-full pointer-events-auto top-2 right-2" @mousedown.stop="handleRounded"></div>
-		<div class="absolute top-[-15px] right-0 cursor-move pointer-events-auto" @mousedown.stop="handleMove" v-if="movable">
-			<FeatherIcon name="move" class="w-3 h-3 text-gray-800"></FeatherIcon>
+		<div class="absolute border-radius-resize w-[10px] h-[10px] border-[1px] border-blue-400 bg-white rounded-full pointer-events-auto top-2 left-2 hidden cursor-default" @mousedown.stop="handleRounded" v-if="roundable">
+			<div class="absolute w-[4px] h-[4px] bg-blue-400 top-[2px] left-[2px] rounded-full pointer-events-none"></div>
 		</div>
 		<div class="absolute w-[4px] border-none bg-transparent top-0
 			bottom-0 left-[-2px] left-handle cursor-ew-resize pointer-events-auto" >
@@ -38,11 +30,10 @@
 	</div>
 </template>
 <script setup>
-import { defineProps, onMounted, ref, onUnmounted, getCurrentInstance } from "vue";
+import { onMounted, ref, onUnmounted, getCurrentInstance } from "vue";
 import useStore from "../store";
-import { Dropdown } from "frappe-ui";
 
-const props = defineProps(["movable"]);
+const props = defineProps(["movable", "resizable", "roundable"]);
 
 const store = useStore();
 const editor = ref(null);
@@ -226,28 +217,56 @@ function updateEditor() {
 	editorWrapper.style.right = `${bound.right}px`;
 }
 
-
-const setType = (type) => {
-	currentInstance.parent.props.elementProperties.element = type;
-	console.log(currentInstance.parent.refs)
-	console.log(target, currentInstance.parent.refs.component)
-}
-
 const handleRounded = (ev) => {
-	let startX = ev.clientX;
-	let startY = ev.clientY;
+	const startX = ev.clientX;
+	const startY = ev.clientY;
+	const handle = ev.currentTarget;
+	const handleStyle = window.getComputedStyle(handle);
+	const initialLeft = 10;
+	const initialTop = 10;
+
+	const targetBounds = target.getBoundingClientRect();
+	const targetWidth = targetBounds.width;
+	const targetHeight = targetBounds.height;
+
+	const maxMovement = Math.min(targetHeight, targetWidth) / 2;
 
 	// to disable cursor jitter
 	const docCursor = document.body.style.cursor;
-	document.body.style.cursor = window.getComputedStyle(ev.target).cursor;
+	document.body.style.cursor = handle.style.cursor;
+
+	let lastX = startX;
+	let lastY = startY;
 
 	const mousemove = (mouseMoveEvent) => {
 		mouseMoveEvent.preventDefault();
-		const movementX = mouseMoveEvent.clientX - startX;
-		const movementY = mouseMoveEvent.clientY - startY;
-		mouseMoveEvent.target.style.top = movementY + "px";
-		mouseMoveEvent.target.style.right = movementX + "px";
-		target.style.borderRadius = movementX + "px";
+		const movementX = (mouseMoveEvent.clientX - lastX);
+		const movementY = (mouseMoveEvent.clientY - lastY);
+
+		let directionX = movementX > 0 ? 'right' : 'left';
+		let directionY = movementY > 0 ? 'down' : 'up';
+
+		// mean of movement on both axis
+		let movement = (movementX + movementY) / 2;
+
+		let newTop = parseInt(handleStyle.top) + movement;
+		let newLeft = parseInt(handleStyle.left) + movement;
+
+		if (newTop < initialTop) {
+			newTop = initialTop;
+		}
+		if (newLeft < initialLeft) {
+			newLeft = initialLeft;
+		}
+
+		let radius = Math.min(newTop - initialTop, newLeft - initialLeft);
+		target.style.borderRadius = `${radius}px`;
+
+		handle.style.top = `${newTop}px`;
+		handle.style.left = `${newLeft}px`;
+
+		lastX = mouseMoveEvent.clientX;
+		lastY = mouseMoveEvent.clientY;
 	};
 	document.addEventListener("mousemove", mousemove);
 	document.addEventListener("mouseup", (mouseUpEvent) => {
