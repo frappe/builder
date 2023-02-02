7d1e607091420d843ce8a9f2b77c8dce69df8d45
Suraj Shetty <surajshetty3416@gmail.com>
2023-02-02 10:08:34 +0530

    fix: Block copy

--- frontend/src/components/BuilderCanvas.vue
+++ frontend/src/components/BuilderCanvas.vue
@@ -7,18 +7,15 @@
 		<div class="canvas absolute min-h-full h-fit bg-white rounded-md overflow-hidden"
 			:style="'width: ' + store.getActiveBreakpoint() + 'px;'" ref="canvas">
 			<draggable :list="store.blocks" :group="{ name: 'blocks' }" item-key="id"
-				class="h-full w-auto flex-col block-container min-h-[300px] mx">
+				class="h-full w-auto flex-col block-container min-h-[300px]">
 				<template #item="{ element }">
-					<BuilderBlock :element-properties="element" @dragstart="setCopyData($event, element, i)"
-						@drag.end="copy">
-					</BuilderBlock>
+					<BuilderBlock :element-properties="element"></BuilderBlock>
 				</template>
 			</draggable>
 		</div>
 	</div>
 </template>
 <script setup>
-import { useDebounceFn } from "@vueuse/core";
 import { onMounted, ref } from "vue";
 import draggable from "vuedraggable";
 import useStore from "../store";
@@ -26,63 +23,57 @@ import setPanAndZoom from "../utils/panAndZoom";
 import BuilderBlock from "./BuilderBlock.vue";
 
 const store = useStore();
-
-const setCopyData = useDebounceFn((event, data) => {
-	if (event.altKey) {
-		event.dataTransfer.action = "copy";
-		event.dataTransfer.data_to_copy = JSON.parse(JSON.stringify(data));
-	}
-});
-const copy = useDebounceFn((event) => {
-	if (event.dataTransfer.action === "copy") {
-		store.blocks.push(event.dataTransfer.data_to_copy);
-	}
-});
-
 const canvasContainer = ref(null);
 const canvas = ref(null);
 
 function getPageData() {
-	function getAttributes(block) {
-		const attributes = {};
-		const skippedAttributes = {};
-		const attributesToSkip = ["contenteditable", "draggable", "style"];
-
-		for (let i = 0; i < block.attributes.length; i += 1) {
-			if (attributesToSkip.includes(block.attributes[i].name)) {
-				skippedAttributes[block.attributes[i].name] = block.attributes[i].value;
-			} else {
-				attributes[block.attributes[i].name] = block.attributes[i].value;
-			}
-		}
-		return { attributes, skippedAttributes };
-	}
-
-	function getBlocks(element) {
-		const blocks = [];
-		element.childNodes.forEach((node) => {
-			if (node.nodeType === Node.ELEMENT_NODE) {
-				const { attributes, skippedAttributes } = getAttributes(node);
-				blocks.push({
-					element: node.tagName.toLowerCase(),
-					attributes,
-					skippedAttributes,
-					styles: node.getAttribute("style"),
-					children: getBlocks(node),
-				});
-			} else if (node.nodeType === Node.TEXT_NODE) {
-				blocks.push({
-					node_type: "Text",
-					node_value: node.nodeValue,
-				});
-			}
-		});
-		return blocks;
-	}
 	return getBlocks(canvas.value);
 }
 
+function getBlocks(element) {
+	const blocks = [];
+	element.childNodes.forEach((node) => {
+
+		if (node.nodeType === Node.ELEMENT_NODE) {
+			blocks.push(getBlockData(node));
+		} else if (node.nodeType === Node.TEXT_NODE && node.nodeValue.trim()) {
+			blocks.push({
+				node_type: "Text",
+				node_value: node.nodeValue,
+			});
+		}
+	});
+	return blocks;
+}
+
+function getAttributes(block) {
+	const attributes = {};
+	const skippedAttributes = {};
+	const attributesToSkip = ["contenteditable", "draggable", "style"];
+
+	for (let i = 0; i < block.attributes.length; i += 1) {
+		if (attributesToSkip.includes(block.attributes[i].name)) {
+			skippedAttributes[block.attributes[i].name] = block.attributes[i].value;
+		} else {
+			attributes[block.attributes[i].name] = block.attributes[i].value;
+		}
+	}
+	return { attributes, skippedAttributes };
+}
+
+function getBlockData(node) {
+	const { attributes, skippedAttributes } = getAttributes(node);
+	return {
+		element: node.tagName.toLowerCase(),
+		attributes,
+		skippedAttributes,
+		styles: node.getAttribute("style"),
+		children: getBlocks(node),
+	}
+}
+
 store.getPageData = getPageData;
+store.getBlockData = getBlockData;
 
 onMounted(() => {
 	setPanAndZoom(canvas.value);
@@ -90,9 +81,13 @@ onMounted(() => {
 
 const clearSelectedComponent = () => {
 	store.selectedComponent = null;
+	document.activeElement.blur();
 };
 
 document.addEventListener("keydown", (e) => {
+	if (e.target.tagName === "INPUT" || e.target.tagName === "TEXTAREA") {
+		return;
+	}
 	if (e.key === "Backspace" && store.selectedComponent && !e.target.closest(".__builder_component__")) {
 		store.blocks = store.blocks.filter((block) => block.id !== store.selectedComponent.element_id);
 		clearSelectedComponent();
@@ -117,5 +112,4 @@ onMounted(() => {
 	}
 	canvas.value.style.transform = `translate(${canvas.value.previousX || 0}px, ${canvas.value.previousY || 0}px) scale(${canvas.value.previousScale})`;
 });
-
 </script>
