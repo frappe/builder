fa9407d04854524d2263d5c7f027271412ef8656
Suraj Shetty <surajshetty3416@gmail.com>
2023-02-13 22:35:17 +0530

    fix: Select block instead of element
    and remove unnecessary code

--- frontend/src/components/BuilderBlock.vue
+++ frontend/src/components/BuilderBlock.vue
@@ -1,27 +1,25 @@
 <template>
 	{{ elementProperties.node_type === 'Text' ? elementProperties.node_value : '' }}
 	<component v-if="elementProperties.node_type !== 'Text'" :is="elementProperties.element"
-		class="relative __builder_component__"
-		@click.exact.stop="selectComponent($event, elementProperties)" @dblclick.stop
-		v-bind="{ ...elementProperties.attributes, ...elementProperties.skippedAttributes }"
-		:styles="styles"
-		ref="component">
+		class="relative __builder_component__" @click.exact.stop="selectComponent($event, elementProperties)"
+		@dblclick.stop v-bind="{ ...elementProperties.attributes, ...elementProperties.skippedAttributes }"
+		:style="styles" ref="component">
 		{{ elementProperties.innerText }}
 		<BuilderBlock :element-properties="element" v-for="element in elementProperties.children"></BuilderBlock>
 	</component>
 	<teleport to="#draggables" v-if="elementProperties.element === 'section'">
-		<BlockDraggables :element-properties="elementProperties" v-bind="{...$attrs}"></BlockDraggables>
+		<BlockDraggables v-if="store.selectedBlocks.includes(elementProperties)" :element-properties="elementProperties"
+			v-bind="{ ...$attrs }"></BlockDraggables>
 	</teleport>
 	<teleport to='#overlay' v-if="elementProperties.node_type !== 'Text'">
-		<BlockEditor :movable="elementProperties.element === 'span'"
-			:roundable="elementProperties.element === 'section'"
-			:resizableX="true"
-			:resizableY="elementProperties.element !== 'img'"
-			:resizable="true"></BlockEditor>
+		<BlockEditor v-if="store.selectedBlocks.includes(elementProperties)"
+			:movable="elementProperties.element === 'span'" :roundable="elementProperties.element === 'section'"
+			:resizableX="true" :resizableY="elementProperties.element !== 'img'" :selected="true" :resizable="true">
+		</BlockEditor>
 	</teleport>
 </template>
 <script setup>
-import { onMounted, ref } from "vue";
+import { onMounted, ref, reactive } from "vue";
 import useStore from "../store";
 import BlockEditor from "./BlockEditor.vue";
 import BlockDraggables from "./BlockDraggables.vue";
@@ -29,22 +27,22 @@ import BlockDraggables from "./BlockDraggables.vue";
 const component = ref(null);
 const store = useStore();
 const props = defineProps(["element-properties"]);
-const styles = ref(props.elementProperties.styles);
+const styles = reactive(props.elementProperties.styles);
 
 onMounted(() => {
 	if (props.elementProperties.node_type === "Text") {
 		return;
 	}
-	store.selectedComponent = component.value;
 	if (!props.elementProperties.id) {
 		props.elementProperties.id = store.generateId();
 	}
-	store.selectedComponent.element_id = props.elementProperties.id;
+	selectComponent(null, props.elementProperties);
 });
 
 const selectComponent = (e, elementProperties) => {
-	store.selectedComponent = e.target.closest(".__builder_component__");
-	store.selectedComponent.element_id = elementProperties.id;
+	// store.selectedComponent = e.target.closest(".__builder_component__");
+	// store.selectedComponent.element_id = elementProperties.id;
+	store.selectedBlocks = [elementProperties];
 };
 
 </script>
