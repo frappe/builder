2a5cb13bed17b920e862f421a9cc9d76e3f3aed1
Suraj Shetty <surajshetty3416@gmail.com>
2023-02-15 17:38:55 +0530

    fix: Page builder layout

--- frontend/src/pages/PageBuilder.vue
+++ frontend/src/pages/PageBuilder.vue
@@ -1,15 +1,15 @@
 <template>
-	<div class="flex-col bg-gray-100">
+	<div class="flex-col bg-gray-100 page-builder">
 		<BuilderToolbar class="relative z-30"></BuilderToolbar>
 		<div>
 			<BuilderSidebar
-				class="fixed left-0 top-[3.5rem] bottom-0 z-20 hidden w-1/6 overflow-auto border-r-2 border-gray-100 bg-white p-5 lg:block">
+				class="fixed left-0 top-[var(--toolbar-height)] bottom-0 z-20 w-[var(--left-panel-width)] overflow-auto border-r-2 border-gray-100 bg-white p-5">
 			</BuilderSidebar>
 			<BuilderCanvas
-				class="canvas-container absolute left-0 right-0 top-[3.5rem] bottom-0 flex justify-center overflow-hidden bg-gray-100 p-10">
+				class="canvas-container absolute left-[var(--left-panel-width)] right-[var(--right-panel-width)] top-[var(--toolbar-height)] bottom-0 flex justify-center overflow-hidden bg-gray-100 p-10">
 			</BuilderCanvas>
 			<BlockProperties
-				class="fixed right-0 top-[3.5rem] bottom-0 z-20 w-1/5 overflow-auto border-l-2 border-gray-100 bg-white p-5 pr-2">
+				class="fixed right-0 top-[var(--toolbar-height)] bottom-0 z-20 w-[var(--right-panel-width)] overflow-auto border-l-2 border-gray-100 bg-white p-5 pr-2">
 			</BlockProperties>
 		</div>
 	</div>
@@ -21,3 +21,11 @@ import BuilderToolbar from "../components/BuilderToolbar.vue";
 import BuilderSidebar from "../components/BuilderSidebar.vue";
 import BlockProperties from "../components/BlockProperties.vue";
 </script>
+
+<style>
+.page-builder {
+	--left-panel-width: 14rem;
+	--right-panel-width: 20rem;
+	--toolbar-height: 3.5rem;
+}
+</style>
