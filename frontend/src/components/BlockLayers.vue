a2ed37af44dbd0122f512fccc5da66008fc69b81
Suraj Shetty <surajshetty3416@gmail.com>
2023-02-16 15:51:48 +0530

    feat: [Init] Layers

---
+++ frontend/src/components/BlockLayers.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<div v-for="block in blocks">
+			<div class="ml-2">
+				{{ block.element }}
+				<div v-if="block.children">
+					<BlockLayers :blocks="block.children"></BlockLayers>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script setup>
+defineProps(["blocks"]);
+</script>
--- frontend/src/components/BuilderSidebar.vue
+++ frontend/src/components/BuilderSidebar.vue
@@ -1,31 +1,63 @@
 <template>
 	<div>
-		<div class="mb-8">
-			<h3 class="mb-1 text-gray-600 font-bold text-xs uppercase mb-3">Pages</h3>
-			<div v-if="!Object.keys(store.pages).length"
-				class="italic text-gray-600  text-sm">
+		<div class="flex w-fit rounded-md bg-gray-200 p-[2px] text-sm mb-4">
+			<button
+				class="rounded-md px-3 py-[3px]"
+				@click="currentView = 'Pages'"
+				:class="{ 'bg-white shadow-md': currentView === 'Pages', 'text-gray-700': currentView !== 'Pages' }">
+				Pages
+			</button>
+			<button
+				class="ml-1 rounded-md px-3 py-[3px]"
+				@click="currentView = 'Components'"
+				:class="{ 'bg-white shadow-md': currentView === 'Components', 'text-gray-700': currentView !== 'Components'}">
+				Components
+			</button>
+			<button
+				class="ml-1 rounded-md px-3 py-[3px]"
+				@click="currentView = 'Layers'"
+				:class="{ 'bg-white shadow-md': currentView === 'Layers', 'text-gray-700': currentView !== 'Layers'}">
+				Layers
+			</button>
+		</div>
+		<div class="mb-8" v-if="currentView === 'Pages'">
+			<h3 class="mb-3 text-xs font-bold uppercase text-gray-600">Pages</h3>
+			<div
+				v-if="!Object.keys(store.pages).length"
+				class="text-sm italic text-gray-600">
 				No Saved Pages
 			</div>
-			<div v-for="page, i in store.pages">
+			<div v-for="(page, i) in store.pages">
 				<ul>
-					<li class="mb-1">
-						<a @click="setPage(page)"
-							class="cursor-pointer text-base rounded-md p-2" :class="{'bg-gray-200': store.selectedPage === page.name}">
+					<li class="mb-1 flex items-center rounded-md pl-2 cursor-pointer"
+						:class="{ 'bg-gray-200': store.selectedPage === page.name }"
+						@click="setPage(page)">
+						<FeatherIcon name="globe" class="w-3 h-3 text-gray-600"></FeatherIcon>
+						<a class="p-1 px-2 text-base flex">
 							{{ page.page_name }}
 						</a>
 					</li>
 				</ul>
 			</div>
 		</div>
-		<Widgets class="mb-7"></Widgets>
-		<Templates class="mb-3"></Templates>
+		<div v-if="currentView === 'Components'">
+			<Widgets class="mb-7"></Widgets>
+			<Templates class="mb-3"></Templates>
+		</div>
+		<div v-if="currentView === 'Layers'">
+			<BlockLayers :blocks="store.blocks"></BlockLayers>
+		</div>
 	</div>
 </template>
 <script setup>
+import { ref } from "vue";
 import { createListResource } from "frappe-ui";
 import useStore from "../store";
 import Widgets from "./BuilderWidgets.vue";
 import Templates from "./BuilderTemplates.vue";
+import BlockLayers from "./BlockLayers.vue";
+
+const currentView = ref("Components");
 
 const store = useStore();
 
@@ -57,7 +89,6 @@ const setPage = (e) => {
 	store.route = e.route;
 	store.selectedPage = e.name;
 };
-
 </script>
 
 <style>
