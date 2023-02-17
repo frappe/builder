5fff97e2f54276af0a7ee3155a2fd761dbfdc6d8
Suraj Shetty <surajshetty3416@gmail.com>
2023-02-17 13:09:23 +0530

    refactor: Rename BlockProperties to BlockPropertiesEditor

--- frontend/src/pages/PageBuilder.vue
+++ frontend/src/pages/PageBuilder.vue
@@ -8,9 +8,9 @@
 			<BuilderCanvas
 				class="canvas-container absolute left-[var(--left-panel-width)] right-[var(--right-panel-width)] top-[var(--toolbar-height)] bottom-0 flex justify-center overflow-hidden bg-gray-100 p-10">
 			</BuilderCanvas>
-			<BlockProperties
+			<BlockPropertiesEditor
 				class="fixed right-0 top-[var(--toolbar-height)] bottom-0 z-20 w-[var(--right-panel-width)] overflow-auto border-l-2 border-gray-100 bg-white p-5 pr-2">
-			</BlockProperties>
+			</BlockPropertiesEditor>
 		</div>
 	</div>
 </template>
@@ -19,7 +19,7 @@
 import BuilderCanvas from "../components/BuilderCanvas.vue";
 import BuilderToolbar from "../components/BuilderToolbar.vue";
 import BuilderSidebar from "../components/BuilderSidebar.vue";
-import BlockProperties from "../components/BlockProperties.vue";
+import BlockPropertiesEditor from "../components/BlockPropertiesEditor.vue";
 </script>
 
 <style>
