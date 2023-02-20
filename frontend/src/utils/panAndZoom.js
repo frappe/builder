5238ff3295f178d7a58955185ffa3eafcfc9f882
Suraj Shetty <surajshetty3416@gmail.com>
2023-02-20 12:38:26 +0530

    fix: Do not manipulate scale and translate directly
    - Use state values instead

--- frontend/src/utils/panAndZoom.js
+++ frontend/src/utils/panAndZoom.js
@@ -1,44 +1,37 @@
-function setPanAndZoom(element, zoomLimits = { min: 0.5, max: 2 }) {
-	const initialScale = element.previousScale || 1;
-	const initialX = element.previousX || 0;
-	const initialY = element.previousY || 0;
-	if (element.parentElement) {
-		element.parentElement.addEventListener(
-			"wheel",
-			(e) => {
-				e.preventDefault();
-				let scale = element.previousScale || 1;
-				let x = element.previousX || 0;
-				let y = element.previousY || 0;
+function setPanAndZoom(
+	props,
+	target,
+	panAndZoomAreaElement,
+	zoomLimits = { min: 0.5, max: 2 }
+) {
+	let scale = props.scale || 1;
+	let x = props.translateX || 0;
+	let y = props.translateY || 0;
 
-				if (e.ctrlKey) {
-					scale -= e.deltaY * 0.01;
-					if (scale < zoomLimits.min) scale = zoomLimits.min;
-					if (scale > zoomLimits.max) scale = zoomLimits.max;
-					element.style.transform = `scale(${scale}) translate(${
-						element.previousX || 0
-					}px, ${element.previousY || 0}px)`;
-					element.previousScale = scale;
-				} else {
-					x -= e.deltaX * 2;
-					y -= e.deltaY * 2;
-					element.style.transform = `translate(${x}px, ${y}px) scale(${
-						element.previousScale || 1
-					})`;
-					element.previousX = x;
-					element.previousY = y;
-				}
-			},
-			{ passive: false }
-		);
+	panAndZoomAreaElement.addEventListener(
+		"wheel",
+		(e) => {
+			e.preventDefault();
+			if (e.ctrlKey) {
+				scale -= e.deltaY * 0.01;
+				if (scale < zoomLimits.min) scale = zoomLimits.min;
+				if (scale > zoomLimits.max) scale = zoomLimits.max;
+				props.scale = scale;
+			} else {
+				x -= e.deltaX * 2;
+				y -= e.deltaY * 2;
+				props.translateX = x;
+				props.translateY = y;
+			}
+		},
+		{ passive: false }
+	);
 
-		element.addEventListener("dblclick", () => {
-			element.style.transform = `translate(${initialX}px, ${initialY}px) scale(${initialScale})`;
-			element.previousX = initialX;
-			element.previousY = initialY;
-			element.previousScale = initialScale;
-		});
-	}
+	target.addEventListener("dblclick", () => {
+		props.scale = 1;
+		props.translateX = 0;
+		props.translateY = 0;
+	});
 }
 
 export default setPanAndZoom;
