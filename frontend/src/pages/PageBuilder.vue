7f583cea1e8d638a8e47450a6140ed04054b71e3
Suraj Shetty <surajshetty3416@gmail.com>
2023-03-20 10:07:36 +0530

    fix: Update canvas colors

--- frontend/src/pages/PageBuilder.vue
+++ frontend/src/pages/PageBuilder.vue
@@ -6,7 +6,7 @@
 				class="fixed left-0 top-[var(--toolbar-height)] bottom-0 z-20 w-[var(--left-panel-width)] overflow-auto border-r-2 border-gray-100 bg-white p-5 dark:bg-zinc-900 dark:border-gray-800">
 			</BuilderSidebar>
 			<BuilderCanvas
-				class="canvas-container absolute left-[var(--left-panel-width)] right-[var(--right-panel-width)] top-[var(--toolbar-height)] bottom-0 flex justify-center overflow-hidden bg-gray-100 p-10 dark:bg-gray-800">
+				class="canvas-container absolute left-[var(--left-panel-width)] right-[var(--right-panel-width)] top-[var(--toolbar-height)] bottom-0 flex justify-center overflow-hidden bg-gray-200 p-10 dark:bg-zinc-800">
 			</BuilderCanvas>
 			<BlockPropertiesEditor
 				class="fixed right-0 top-[var(--toolbar-height)] bottom-0 z-20 w-[var(--right-panel-width)] overflow-auto border-l-2 border-gray-100 bg-white p-5 pr-2 dark:bg-zinc-900 dark:border-gray-800">
