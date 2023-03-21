77ddadf341c0c34b0b2394a1314befac60f1c0b8
Suraj Shetty <surajshetty3416@gmail.com>
2023-03-21 23:43:50 +0530

    feat: Make layers draggable

--- frontend/src/components/BlockLayers.vue
+++ frontend/src/components/BlockLayers.vue
@@ -1,28 +1,38 @@
 <template>
 	<div class="ml-1">
-		<div v-for="block in blocks">
-			<div class="cursor-pointer text-base px-2 py-1 truncate dark:text-gray-400 rounded-md border-[1px]"
-			:class="{
-					'border-transparent': store.builderState.selectedBlock !== block && store.hoveredBlock !== block.blockId,
-					'border-blue-200 bg-transparent dark:border-blue-800': store.hoveredBlock === block.blockId,
-					'border-blue-400 bg-transparent dark:border-blue-600': store.builderState.selectedBlock === block,
-				}"
-				@click.stop="selectBlock($event, block)"
-				@mouseover.stop="store.hoveredBlock = block.blockId"
-				@mouseleave.stop="store.hoveredBlock = null">
-				<span class="flex items-center">
-					<FeatherIcon :name="block.getIcon()" class="h-3 w-3 mr-1 text-gray-500"></FeatherIcon>
-					{{ block.element }} {{ block.innerText ?  " | " + block.innerText : '' }}
-				</span>
-				<div v-if="block.children">
-					<BlockLayers :blocks="block.children"></BlockLayers>
-				</div>
-			</div>
-		</div>
+		<draggable
+			class="block-tree"
+			:list="blocks"
+			:group="{ name: 'block-tree' }"
+			item-key="blockId"
+		>
+			<template #item="{ element }">
+				<div class="cursor-pointer text-base pl-2 pr-[2px] py-1 dark:text-gray-400 rounded-md border-[1px]"
+					:class="{
+							'border-transparent': store.builderState.selectedBlock !== block && store.hoveredBlock !== element.blockId,
+							'border-blue-200 bg-transparent dark:border-blue-800': store.hoveredBlock === element.blockId,
+							'border-blue-400 bg-transparent dark:border-blue-600': store.builderState.selectedBlock && store.builderState.selectedBlock.blockId === element.blockId,
+						}"
+						@click.stop="selectBlock($event, element)"
+						@mouseover.stop="store.hoveredBlock = element.blockId"
+						@mouseleave.stop="store.hoveredBlock = null">
+						<span class="flex items-center">
+							<FeatherIcon :name="element.getIcon()" class="h-3 w-3 mr-1 text-gray-500"></FeatherIcon>
+							<span class="truncate">
+								{{ element.element }} {{ element.innerText ?  " | " + element.innerText : '' }}
+							</span>
+						</span>
+						<div v-if="element.children">
+							<BlockLayers :blocks="element.children"></BlockLayers>
+						</div>
+					</div>
+			</template>
+		</draggable>
 	</div>
 </template>
 <script setup>
 import { FeatherIcon } from "frappe-ui";
+import draggable from "vuedraggable";
 import useStore from "../store";
 const store = useStore();
 defineProps(["blocks"]);
