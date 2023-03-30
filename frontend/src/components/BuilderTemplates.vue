7a43fc4c59ff8a10b6397006666ba647effac36b
Suraj Shetty <surajshetty3416@gmail.com>
2023-03-30 22:11:32 +0530

    feat: Template preview

--- frontend/src/components/BuilderTemplates.vue
+++ frontend/src/components/BuilderTemplates.vue
@@ -1,16 +1,24 @@
 <template>
 	<div>
-		<h3 class="mb-3 text-gray-600 font-bold text-xs uppercase">Templates</h3>
-		<draggable :list="components" :group="{ name: 'blocks', pull: 'clone', put: false }" item-key="id"
+		<h3 class="mb-4 text-gray-600 font-bold text-xs uppercase">Templates</h3>
+		<draggable :list="store.components" :group="{ name: 'blocks', pull: 'clone', put: false }" item-key="id"
 			:sort="false"
 			:clone="(obj) => store.getBlockCopy(obj.block)" class="w-full flex flex-wrap">
 			<template #item="{ element }">
-				<div>
+				<div class="mb-3">
 					<div class="flex items-center cursor-pointer justify-center
-						h-8 w-16 border shadow-sm rounded-md mr-2 last:mr-0 mb-2 bg-white dark:bg-gray-800 dark:border-gray-700 dark:text-gray-200">
-						<FeatherIcon :name="element.icon" class="h-4 text-gray-700 dark:text-gray-300" />
+								h-24 w-48 border-2 shadow-sm rounded-md mr-2 last:mr-0 mb-1 bg-white
+								dark:bg-zinc-800 dark:border-zinc-700 dark:text-zinc-200
+								overflow-hidden relative p-2">
+						<div class="absolute pointer-events-none w-[1200px]" :style="{
+							'transform': 'scale(' + element.scale + ')'
+						}">
+							<BuilderBlock :element-properties="store.getBlockCopy(element.block)" ref="preview"
+								@render-complete="(el) => setScale(el, element)" preview="true">
+							</BuilderBlock>
+						</div>
 					</div>
-					<p class="text-xs text-gray-800">{{ element.component_name }}</p>
+					<p class="text-xs text-gray-800 dark:text-zinc-500">{{ element.component_name }}</p>
 				</div>
 			</template>
 		</draggable>
@@ -19,9 +27,13 @@
 <script setup>
 import draggable from "vuedraggable";
 import { createListResource } from "frappe-ui";
+import BuilderBlock from "./BuilderBlock.vue";
 import useStore from "../store";
+import { ref } from "vue";
+
 const store = useStore();
-const components = store.components;
+const preview = ref(null);
+
 createListResource({
 	doctype: "Web Page Component",
 	fields: ["component_name", "icon", "block"],
@@ -35,8 +47,14 @@ createListResource({
 	transform(data) {
 		data.forEach((d) => {
 			d.block = JSON.parse(d.block);
+			d.scale = 0.3; // for preview
 		});
 		return data;
 	},
 });
+
+const setScale = (el, block) => {
+	const scale = Math.min(140 / (el.offsetWidth), 70 / (el.offsetHeight), 0.6);
+	block.scale = scale;
+};
 </script>
