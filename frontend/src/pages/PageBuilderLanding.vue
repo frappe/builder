48ae8a6afff59c27f39d5a2fec62e9fb4bb247b7
Suraj Shetty <surajshetty3416@gmail.com>
2023-04-10 09:10:21 +0530

    feat: [Init] PageBuilder Landing

---
+++ frontend/src/pages/PageBuilderLanding.vue
@@ -0,0 +1,28 @@
+<template>
+	<section class="m-auto flex w-3/4 justify-center pt-10">
+		<div
+			v-for="page of pages"
+			:key="page.page_name"
+			class="mr-2 h-24 w-56 rounded-md border-2 border-gray-200 bg-gray-100 p-2">
+			{{ page.page_name }}
+		</div>
+	</section>
+</template>
+<script setup>
+import { createListResource } from "frappe-ui";
+import { ref } from "vue";
+let pages = ref([]);
+
+createListResource({
+	doctype: "Web Page Beta",
+	fields: ["name", "blocks", "page_name", "route"],
+	orderBy: "creation desc",
+	start: 0,
+	pageLength: 10,
+	auto: true,
+	onSuccess(data) {
+		pages.value = data;
+		// setPage(store.pages[localStorage.getItem("selectedPage") || "home"])
+	},
+});
+</script>
