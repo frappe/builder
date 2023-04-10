9069aac5768b95148b1003905f5e205d0c6066f3
Suraj Shetty <surajshetty3416@gmail.com>
2023-04-10 09:10:21 +0530

    feat: [Init] PageBuilder Landing

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -3,13 +3,22 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
 	{
 		path: "/",
+		redirect: "builder",
+	},
+	{
+		path: "/home",
+		name: "Home",
+		component: () => import("@/pages/PageBuilderLanding.vue"),
+	},
+	{
+		path: "/builder",
 		name: "Page Builder",
 		component: () => import("@/pages/PageBuilder.vue"),
 	},
 ];
 
 const router = createRouter({
-	history: createWebHistory("/frontend"),
+	history: createWebHistory("/"),
 	routes,
 });
 
