16f4cd434953f1de632324df3d8658b1aed11600
Suraj Shetty <surajshetty3416@gmail.com>
2023-04-15 13:44:03 +0530

    feat: [init] Generate Page using ChatGPT

--- website_builder/api.py
+++ website_builder/api.py
@@ -1,6 +1,7 @@
 import frappe
 import json
-import random
+import openai
+from frappe.integrations.utils import make_post_request
 
 @frappe.whitelist(allow_guest=True)
 def publish(blocks, page_name=None):
@@ -17,3 +18,34 @@ def publish(blocks, page_name=None):
 	page.blocks = blocks
 	page.save(ignore_permissions=True)
 	return page
+
+@frappe.whitelist(allow_guest=True)
+def get_blocks(prompt):
+	API_KEY = frappe.conf.openai_api_key
+	if not API_KEY:
+		frappe.throw("OpenAI API Key not set in site config.")
+
+	messages = [
+		{"role": "system", "content": "You are a web developer. You add inline <style> in <body> instead of relying on external stylesheet. You use any publicly available image"},
+		{"role": "user", "content": prompt}
+	]
+
+	response = make_post_request(
+		"https://api.openai.com/v1/chat/completions",
+		headers={
+			"Content-Type": "application/json",
+			"Authorization": f"Bearer {API_KEY}"
+		},
+		data=json.dumps({
+			"model": "gpt-3.5-turbo",
+			"messages": messages,
+		}),
+	)
+	return response['choices'][0]['message']['content']
+
+
+
+
+
+
+
