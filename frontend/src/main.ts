153c4170e5005cf3bc62de1fb1206101e90d8b8a
Suraj Shetty <surajshetty3416@gmail.com>
2023-05-15 08:38:22 +0530

    fix: Dynamically get index style file

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,16 +1,17 @@
-import { createApp } from "vue";
+import App from "@/App.vue";
 import {
-	FrappeUI,
 	Button,
 	FeatherIcon,
-	setConfig,
+	FrappeUI,
 	frappeRequest,
+	setConfig,
 } from "frappe-ui";
 import { createPinia } from "pinia";
-import router from "./router";
-import App from "./App.vue";
+import { createApp } from "vue";
 import "./index.css";
+import router from "./router";
 import "./utils/arrayFunctions";
+
 
 const app = createApp(App);
 const pinia = createPinia();
--- website_builder/website_builder/doctype/web_page_beta/web_page_beta.py
+++ website_builder/website_builder/doctype/web_page_beta/web_page_beta.py
@@ -21,48 +21,51 @@ class WebPageBeta(WebsiteGenerator):
 	def get_context(self, context):
 		# show breadcrumbs
 		context.title = "page"
-		context.fonts = {}
-		content, style = self.get_content(context)
+		content, style, fonts = get_block_html(self.blocks)
+		context.fonts = fonts
 		context.content = content
 		context.style = style
+		context.style_file_path = get_style_file_path()
 
-	def get_content(self, context):
-		soup = bs.BeautifulSoup("", "html.parser")
-		blocks = json.loads(self.blocks)
-		style_tag = soup.new_tag("style")
+def get_block_html(blocks):
+	blocks = frappe.parse_json(blocks)
+	if not isinstance(blocks, list):
+		blocks = [blocks]
+	soup = bs.BeautifulSoup("", "html.parser")
+	style_tag = soup.new_tag("style")
+	font_map = {}
 
-		def get_html(blocks, soup):
-			html = ""
-			def get_tag(node, soup):
-				tag = soup.new_tag(node.get("originalElement") or node["element"])
-				tag.attrs = node.get("attributes", {})
-				classes = node.get("classes", [])
-				if node.get("baseStyles", {}):
-					style_class = f"--{frappe.generate_hash(length=8)}"
-					base_styles = node.get("baseStyles", {})
-					mobile_styles = node.get("mobileStyles", {})
-					tablet_styles = node.get("tabletStyles", {})
-					setFonts([base_styles, mobile_styles, tablet_styles], context)
-					append_style(node.get("baseStyles", {}), style_tag, style_class)
-					append_style(node.get("tabletStyles", {}), style_tag, style_class, device="tablet")
-					append_style(node.get("mobileStyles", {}), style_tag, style_class, device="mobile")
-					classes.append(style_class)
+	def get_html(blocks, soup):
+		html = ""
+		def get_tag(node, soup):
+			tag = soup.new_tag(node.get("originalElement") or node["element"])
+			tag.attrs = node.get("attributes", {})
+			classes = node.get("classes", [])
+			if node.get("baseStyles", {}):
+				style_class = f"--{frappe.generate_hash(length=8)}"
+				base_styles = node.get("baseStyles", {})
+				mobile_styles = node.get("mobileStyles", {})
+				tablet_styles = node.get("tabletStyles", {})
+				setFonts([base_styles, mobile_styles, tablet_styles], font_map)
+				append_style(node.get("baseStyles", {}), style_tag, style_class)
+				append_style(node.get("tabletStyles", {}), style_tag, style_class, device="tablet")
+				append_style(node.get("mobileStyles", {}), style_tag, style_class, device="mobile")
+				classes.append(style_class)
 
-				tag.attrs["class"] = get_class(classes)
-				if node.get("innerText"):
-					tag.append(node.get("innerText"))
+			tag.attrs["class"] = get_class(classes)
+			if node.get("innerText"):
+				tag.append(node.get("innerText"))
 
-				for child in node.get("children", []):
-					tag.append(get_tag(child, soup))
-				return tag
+			for child in node.get("children", []):
+				tag.append(get_tag(child, soup))
+			return tag
 
-			for block in blocks:
-				html += str(get_tag(block, soup))
+		for block in blocks:
+			html += str(get_tag(block, soup))
 
-			return html, str(style_tag)
+		return html, str(style_tag), font_map
 
-		return get_html(blocks, soup)
-
+	return get_html(blocks, soup)
 
 def get_style(style_obj):
 	return "".join(f"{camel_case_to_kebab_case(key)}: {value};" for key, value in style_obj.items()) if style_obj else ""
@@ -85,12 +88,21 @@ def append_style(style_obj, style_tag, style_class, device="desktop"):
 		style_string = f"@media only screen and (min-width: 426px) and (max-width: 768px) {{ {style_string} }}"
 	style_tag.append(style_string)
 
-def setFonts(styles, context):
+def setFonts(styles, font_map):
 	for style in styles:
 		font = style.get("fontFamily")
 		if font:
-			if font in context.fonts:
-				if style.get("fontWeight") and style.get("fontWeight") not in context.fonts[font]["weights"]:
-					context.fonts[font]["weights"].append(style.get("fontWeight"))
+			if font in font_map:
+				if style.get("fontWeight") and style.get("fontWeight") not in font_map[font]["weights"]:
+					font_map[font]["weights"].append(style.get("fontWeight"))
 			else:
-				context.fonts[font] = { "weights": [style.get("fontWeight") or "400"] }
+				font_map[font] = { "weights": [style.get("fontWeight") or "400"] }
+
+def get_style_file_path():
+	# TODO: Redo this, currently it loads the first matching file
+	import glob
+	folder_path = "./assets/website_builder/frontend/assets/"
+	file_pattern = "index.*.css"
+	matching_files = glob.glob(f"{folder_path}/{file_pattern}")
+	if matching_files:
+		return matching_files[0].lstrip(".")
