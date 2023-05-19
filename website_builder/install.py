5041592a361c1e3a30face12044f68356d7c4a4b
Suraj Shetty <surajshetty3416@gmail.com>
2023-05-19 17:58:28 +0530

    fix: Install command

--- website_builder/install.py
+++ website_builder/install.py
@@ -2,4 +2,4 @@ import frappe
 
 
 def after_install():
-	frappe.utils.execute_in_shell("playwright install chromium")
+	frappe.utils.execute_in_shell("PLAYWRIGHT_BROWSERS_PATH=0 playwright install chromium")
