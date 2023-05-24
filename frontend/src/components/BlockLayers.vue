e3436c5232470bb99591c658d530e63d0c61198e
Suraj Shetty <surajshetty3416@gmail.com>
2023-05-24 12:30:11 +0530

    refactor: Move some basic funtionalities to block instance

--- frontend/src/components/BlockLayers.vue
+++ frontend/src/components/BlockLayers.vue
@@ -5,19 +5,11 @@
 				<div
 					class="cursor-pointer rounded-md border-[1px] bg-white py-1 pl-2 pr-[2px] text-base text-gray-600 dark:bg-zinc-900"
 					:class="{
-						// TODO: simplify this
 						'border-transparent text-gray-700 dark:text-gray-500':
-							(store.builderState.selectedBlock &&
-								store.builderState.selectedBlock.blockId !== element.blockId &&
-								store.hoveredBlock !== element.blockId) ||
-							!store.builderState.selectedBlock,
+							!element.isSelected() && !element.isHovered(),
 						'border-blue-200 text-gray-600 dark:border-blue-800 dark:text-gray-500':
-							store.hoveredBlock === element.blockId &&
-							store.builderState.selectedBlock &&
-							store.builderState.selectedBlock.blockId !== element.blockId,
-						'border-blue-400 text-gray-900 dark:border-blue-600 dark:text-gray-200':
-							store.builderState.selectedBlock &&
-							store.builderState.selectedBlock.blockId === element.blockId,
+							element.isHovered() && !element.isSelected(),
+						'border-blue-400 text-gray-900 dark:border-blue-600 dark:text-gray-200': element.isSelected(),
 					}"
 					@click.stop="selectBlock(element)"
 					@mouseover.stop="store.hoveredBlock = element.blockId"
@@ -29,7 +21,8 @@
 							v-if="element.children && element.children.length && !element.isRoot()"
 							@click.stop="element.fold = !element.fold" />
 						<FeatherIcon :name="element.getIcon()" class="mr-1 h-3 w-3" />
-						<span class="truncate min-h-[1em] min-w-[2em]"
+						<span
+							class="min-h-[1em] min-w-[2em] truncate"
 							:contenteditable="element.editable"
 							@dblclick="element.editable = true"
 							@keyup.enter.stop.prevent="element.editable = false"
@@ -71,5 +64,5 @@ const setBlockName = (ev: Event, block: LayerBlock) => {
 	const target = ev.target as HTMLElement;
 	block.blockName = target.innerText;
 	block.editable = false;
-}
+};
 </script>
