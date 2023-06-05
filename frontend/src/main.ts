2ebfa88ccced234a99088c492388476c877bc228
Suraj Shetty <surajshetty3416@gmail.com>
2023-06-05 14:11:50 +0530

    fix: Make input component globally available

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,5 @@
 import App from "@/App.vue";
-import { Button, FeatherIcon, FrappeUI, frappeRequest, setConfig } from "frappe-ui";
+import { Button, FeatherIcon, FrappeUI, frappeRequest, setConfig, Input } from "frappe-ui";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 import "./index.css";
@@ -17,6 +17,7 @@ app.use(pinia);
 
 // eslint-disable-next-line vue/multi-word-component-names, vue/no-reserved-component-names
 app.component("Button", Button);
+app.component("Input", Input);
 
 app.component("FeatherIcon", FeatherIcon);
 app.mount("#app");
