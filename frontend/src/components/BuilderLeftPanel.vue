0ec345f8840a3dca1d3a7ee299fdd4b02fe0e958
Suraj Shetty <surajshetty3416@gmail.com>
2023-06-06 14:42:08 +0530

    refactor: Remove widgets panel

--- frontend/src/components/BuilderLeftPanel.vue
+++ frontend/src/components/BuilderLeftPanel.vue
@@ -22,10 +22,10 @@
 			@resize="(width) => (store.builderLayout.leftPanelWidth = width)" />
 		<div class="flex w-full border-gray-200 p-[2px] text-sm dark:border-zinc-800">
 			<button
-				v-for="tab of ['Widgets', 'Components', 'Layers']"
+				v-for="tab of ['Layers', 'Components']"
 				:key="tab"
 				class="mx-3 flex-1 p-2"
-				@click="store.sidebarActiveTab = tab"
+				@click="store.sidebarActiveTab = tab as 'Layers' | 'Components'"
 				:class="{
 					'border-b-[1px] border-gray-900 dark:border-zinc-500 dark:text-zinc-300':
 						store.sidebarActiveTab === tab,
@@ -33,29 +33,6 @@
 				}">
 				{{ tab }}
 			</button>
-		</div>
-		<div class="mb-8" v-show="store.sidebarActiveTab === 'Pages'">
-			<h3 class="mb-3 text-xs font-bold uppercase text-gray-600">Pages</h3>
-			<div v-if="!Object.keys(store.pages).length" class="text-sm italic text-gray-600">No Saved Pages</div>
-			<div v-for="page of store.pages" :key="page.page_name">
-				<ul>
-					<li
-						class="mb-1 flex cursor-pointer items-center rounded-md pl-2 font-medium text-gray-600 dark:text-gray-500"
-						:class="{
-							'bg-gray-200 text-gray-900 dark:bg-zinc-800 dark:text-gray-200':
-								store.builderState.selectedPage === page.name,
-						}"
-						@click="setPage(page)">
-						<FeatherIcon name="globe" class="h-3 w-3" />
-						<a class="flex p-1 px-2 text-base">
-							{{ page.page_name }}
-						</a>
-					</li>
-				</ul>
-			</div>
-		</div>
-		<div v-show="store.sidebarActiveTab === 'Widgets'">
-			<Widgets class="p-4" />
 		</div>
 		<div v-show="store.sidebarActiveTab === 'Components'">
 			<Components class="p-4" />
@@ -72,7 +49,6 @@ import { Ref, ref } from "vue";
 import useStore from "../store";
 import BlockLayers from "./BlockLayers.vue";
 import Components from "./BuilderComponents.vue";
-import Widgets from "./BuilderWidgets.vue";
 import PanelResizer from "./PanelResizer.vue";
 
 import { useRouter } from "vue-router";
