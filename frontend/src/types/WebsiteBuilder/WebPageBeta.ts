47dd2e2c52ec0464569ce115a50414805b7e3858
Suraj Shetty <surajshetty3416@gmail.com>
2023-06-06 22:17:23 +0530

    fix: Set page title from web page doc
    - Also add .ts file for Web Page Beta doctype

---
+++ frontend/src/types/WebsiteBuilder/WebPageBeta.ts
@@ -0,0 +1,24 @@
+export interface WebPageBeta {
+	creation: string
+	name: string
+	modified: string
+	owner: string
+	modified_by: string
+	docstatus: 0 | 1 | 2
+	parent?: string
+	parentfield?: string
+	parenttype?: string
+	idx?: number
+	/**	Page Name : Data	*/
+	page_name?: string
+	/**	Page Title : Data	*/
+	page_title?: string
+	/**	Route : Data	*/
+	route?: string
+	/**	Published : Check	*/
+	published?: 0 | 1
+	/**	Blocks : JSON	*/
+	blocks?: any
+	/**	Preview : Attach Image	*/
+	preview?: string
+}
