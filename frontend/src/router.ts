b2f31e76ce247f51e6f030d97fcb4489b535a65e
Suraj Shetty <surajshetty3416@gmail.com>
2023-06-08 16:57:16 +0530

    fix: Validate if user is logged in

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,30 +1,46 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+// Hack, TODO: Check authentication
+const validateVisit = function(to, from, next) {
+	if (document.cookie.includes("user_id") && !document.cookie.includes("user_id=Guest")) {
+		next()
+	} else {
+		window.location.href = "/login";
+	}
+};
+
 const routes = [
 	{
 		path: "/",
+		beforeEnter: validateVisit,
 		redirect: "/home",
 	},
 	{
 		path: "/builder",
+		beforeEnter: validateVisit,
 		redirect: "/builder/new",
 	},
 	{
 		path: "/home",
 		name: "home",
+		beforeEnter: validateVisit,
 		component: () => import("@/pages/PageBuilderLanding.vue"),
 	},
 	{
 		path: "/builder/:pageId",
 		name: "builder",
+		beforeEnter: validateVisit,
 		component: () => import("@/pages/PageBuilder.vue"),
 	},
 	{
 		path: "/builder/:pageId/settings",
 		name: "page-settings",
+		beforeEnter: validateVisit,
 		component: () => import("@/pages/PageSettings.vue"),
 	}
 ];
+
+
 
 const router = createRouter({
 	history: createWebHistory("/p"),
