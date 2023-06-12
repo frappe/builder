ea0371ae5d54b00c182cf557962594b9eec14362
Suraj Shetty <surajshetty3416@gmail.com>
2023-06-12 22:20:58 +0530

    fix: Set homepage for website users

--- website_builder/hooks.py
+++ website_builder/hooks.py
@@ -38,12 +38,9 @@ app_license = "MIT"
 # ----------
 
 # application home page (will override Website Settings)
-# home_page = "login"
+home_page = "/p/home"
 
 # website user home page (by Role)
-# role_home_page = {
-#	"Role": "home_page"
-# }
 
 # Generators
 # ----------
