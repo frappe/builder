3956302330f3995101083d011e5eec2aa36f2575
Aditya Hase <aditya@adityahase.com>
2023-06-13 14:53:59 +0530

    fix: Download and Install Chromium for Playwright

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
+import os
+import atexit
+import subprocess
 from setuptools import find_packages, setup
+from setuptools.command.develop import develop
 
 with open("requirements.txt") as f:
 	install_requires = f.read().strip().split("\n")
 
 # get version from __version__ variable in website_builder/__init__.py
 from website_builder import __version__ as version
+
+
+def install_playwright():
+	python_path = os.path.join("..", "..", "env", "bin", "python")
+	print(subprocess.run(f"{python_path} -m playwright install", shell=True))
+
+
+class RunDevelopCommand(develop):
+	def __init__(self, *args, **kwargs):
+		super(RunDevelopCommand, self).__init__(*args, **kwargs)
+		# This is a hack to ensure that the command is run
+		# after all the dependencies are installed
+		atexit.register(install_playwright)
+
 
 setup(
 	name="website_builder",
@@ -14,5 +32,8 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=install_requires
+	install_requires=install_requires,
+	cmdclass={
+		"develop": RunDevelopCommand,
+	},
 )
