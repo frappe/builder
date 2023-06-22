d119f2e222e55fdd06c47d4b68d8e3d6cb54a928
Suraj Shetty <surajshetty3416@gmail.com>
2023-06-22 13:07:22 +0530

    fix: Add fallback image to color import

--- frontend/src/components/ColorInput.vue
+++ frontend/src/components/ColorInput.vue
@@ -1,19 +1,24 @@
 <template>
 	<ColorPicker :modelValue="value" @update:model-value="(color) => emit('change', color)">
 		<template #target="{ togglePopover, isOpen }">
-			<div class="relative mt-5">
-				<div
-					class="absolute left-2 top-[6px] z-10 h-4 w-4 rounded"
-					@click="togglePopover"
-					:style="{
-						backgroundColor: value,
-					}"></div>
-				<Input
-					type="text"
-					placeholder="Select Color"
-					inputClass="pl-8"
-					:value="value"
-					@change="emit('change')"></Input>
+			<div class="mt-5 flex items-center justify-between">
+				<span class="inline-block text-[10px] font-medium uppercase text-gray-600 dark:text-zinc-400">
+					<slot />
+				</span>
+				<div class="relative w-[150px]">
+					<div
+						class="absolute left-2 top-[6px] z-10 h-4 w-4 rounded"
+						@click="togglePopover"
+						:style="{
+							background: value ? value : `url(/color-circle.png) center / contain`,
+						}"></div>
+					<Input
+						type="text"
+						placeholder="Select Color"
+						inputClass="pl-8"
+						:value="value"
+						@change="emit('change')"></Input>
+				</div>
 			</div>
 		</template>
 	</ColorPicker>
