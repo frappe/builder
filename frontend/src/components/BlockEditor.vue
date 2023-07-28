449ec50cf2679f4ac95f19c3b42ac8a8a36bd87c
Suraj Shetty <surajshetty3416@gmail.com>
2023-07-28 11:51:23 +0530

    refactor: Move context menu options to separate file and reuse for layers

--- frontend/src/components/BlockEditor.vue
+++ frontend/src/components/BlockEditor.vue
@@ -1,83 +1,51 @@
 <template>
-	<div
-		class="editor pointer-events-none fixed z-[19] box-content select-none border-[1px]"
-		ref="editor"
-		@click="handleClick"
-		@dblclick="handleDoubleClick"
-		@mousedown.prevent="handleMove"
-		@contextmenu="showContextMenu"
-		:data-block-id="block.blockId"
-		:class="getStyleClasses">
-		<BlockDescription v-if="isBlockSelected && !resizing && !editable" :block="block"></BlockDescription>
-		<PaddingHandler
-			v-if="isBlockSelected && !resizing && !editable && store.builderState.selectedBlocks.length === 1"
-			:target-block="block"
-			:on-update="updateTracker"
-			:disable-handlers="false"
-			:breakpoint="breakpoint" />
-		<MarginHandler
-			v-if="isBlockSelected && !resizing && !editable && store.builderState.selectedBlocks.length === 1"
-			:target-block="block"
-			:on-update="updateTracker"
-			:disable-handlers="false"
-			:breakpoint="breakpoint" />
-		<BorderRadiusHandler
-			v-if="isBlockSelected && !block.isRoot() && !editable && store.builderState.selectedBlocks.length === 1"
-			:target-block="block"
-			:target="target" />
-		<BoxResizer v-if="showResizer" :targetBlock="block" @resizing="resizing = $event" :target="target" />
-		<ContextMenu
-			v-if="contextMenuVisible"
-			:pos-x="posX"
-			:pos-y="posY"
-			:options="contextMenuOptions"
-			@select="handleContextMenuSelect"
-			v-on-click-outside="() => (contextMenuVisible = false)" />
-		<Dialog
-			style="z-index: 40"
-			:options="{
-				title: 'New Component',
-				size: 'sm',
-				actions: [
-					{
-						label: 'Save',
-						appearance: 'primary',
-						onClick: createComponentHandler,
-					},
-					{ label: 'Cancel' },
-				],
-			}"
-			v-model="showDialog">
-			<template #body-content>
-				<Input type="text" v-model="componentProperties.componentName" label="Component Name" required />
-				<div class="mt-3">
-					<Input
-						class="text-sm [&>span]:!text-sm"
-						type="checkbox"
-						v-model="componentProperties.isDynamicComponent"
-						label="Is Dynamic" />
-				</div>
-			</template>
-		</Dialog>
-	</div>
+	<BlockContextMenu :block="block" :editable="editable" v-slot="{ onContextMenu }">
+		<div
+			class="editor pointer-events-none fixed z-[19] box-content select-none border-[1px]"
+			ref="editor"
+			@click="handleClick"
+			@dblclick="handleDoubleClick"
+			@mousedown.prevent="handleMove"
+			@contextmenu="onContextMenu"
+			:data-block-id="block.blockId"
+			:class="getStyleClasses">
+			<BlockDescription v-if="isBlockSelected && !resizing && !editable" :block="block"></BlockDescription>
+			<PaddingHandler
+				v-if="isBlockSelected && !resizing && !editable && store.builderState.selectedBlocks.length === 1"
+				:target-block="block"
+				:on-update="updateTracker"
+				:disable-handlers="false"
+				:breakpoint="breakpoint" />
+			<MarginHandler
+				v-if="isBlockSelected && !resizing && !editable && store.builderState.selectedBlocks.length === 1"
+				:target-block="block"
+				:on-update="updateTracker"
+				:disable-handlers="false"
+				:breakpoint="breakpoint" />
+			<BorderRadiusHandler
+				v-if="
+					isBlockSelected && !block.isRoot() && !editable && store.builderState.selectedBlocks.length === 1
+				"
+				:target-block="block"
+				:target="target" />
+			<BoxResizer v-if="showResizer" :targetBlock="block" @resizing="resizing = $event" :target="target" />
+		</div>
+	</BlockContextMenu>
 </template>
 <script setup lang="ts">
 import Block from "@/utils/block";
-import { vOnClickOutside } from "@vueuse/components";
+import { addPxToNumber } from "@/utils/helpers";
 import { Ref, computed, inject, nextTick, onMounted, ref, watch, watchEffect } from "vue";
 
-import { addPxToNumber, getNumberFromPx } from "@/utils/helpers";
-import { Dialog, Input, createResource } from "frappe-ui";
 import useStore from "../store";
 import setGuides from "../utils/guidesTracker";
 import trackTarget from "../utils/trackTarget";
+import BlockContextMenu from "./BlockContextMenu.vue";
 import BlockDescription from "./BlockDescription.vue";
 import BorderRadiusHandler from "./BorderRadiusHandler.vue";
 import BoxResizer from "./BoxResizer.vue";
-import ContextMenu from "./ContextMenu.vue";
 import MarginHandler from "./MarginHandler.vue";
 import PaddingHandler from "./PaddingHandler.vue";
-import webComponent from "@/data/webComponent";
 
 const canvasProps = inject("canvasProps") as CanvasProps;
 
@@ -191,10 +159,6 @@ const handleClick = (ev: MouseEvent) => {
 	if (element.classList.contains("__builder_component__")) {
 		element.dispatchEvent(new MouseEvent("click", ev));
 	}
-	// if (store.builderState.mode === "select") {
-	// 	editorWrapper.classList.remove("pointer-events-none");
-	// 	editorWrapper.classList.add("pointer-events-auto");
-	// }
 };
 
 const handleDoubleClick = () => {
@@ -246,86 +210,4 @@ const handleMove = (ev: MouseEvent) => {
 		{ once: true }
 	);
 };
-
-// Context menu
-const contextMenuVisible = ref(false);
-const posX = ref(0);
-const posY = ref(0);
-const showDialog = ref(false);
-const componentProperties = ref({
-	componentName: "",
-	isDynamicComponent: 0,
-});
-
-const showContextMenu = (event: MouseEvent) => {
-	if (props.block.isRoot() || props.editable) return;
-	contextMenuVisible.value = true;
-	posX.value = event.pageX;
-	posY.value = event.pageY;
-	event.preventDefault();
-	event.stopPropagation();
-};
-
-const handleContextMenuSelect = (action: CallableFunction) => {
-	action();
-	contextMenuVisible.value = false;
-};
-
-const copyStyle = () => {
-	store.copiedStyle = {
-		blockId: props.block.blockId,
-		style: props.block.getStylesCopy(),
-	};
-};
-
-const createComponentHandler = ({ close }: { close: () => void }) => {
-	const blockCopy = store.getBlockCopy(props.block);
-	blockCopy.removeStyle("left");
-	blockCopy.removeStyle("top");
-	blockCopy.removeStyle("position");
-	webComponent.insert
-		.submit({
-			block: blockCopy,
-			component_name: componentProperties.value.componentName,
-			is_dynamic: componentProperties.value.isDynamicComponent,
-		})
-		.then(() => {
-			store.sidebarActiveTab = "Components";
-		});
-	close();
-};
-
-const duplicateBlock = () => {
-	const blockCopy = store.getBlockCopy(props.block);
-	const parentBlock = props.block.getParentBlock();
-
-	if (blockCopy.getStyle("position") === "absolute") {
-		// shift the block a bit
-		const left = getNumberFromPx(blockCopy.getStyle("left"));
-		const top = getNumberFromPx(blockCopy.getStyle("top"));
-		blockCopy.setStyle("left", `${left + 20}px`);
-		blockCopy.setStyle("top", `${top + 20}px`);
-	}
-
-	if (parentBlock) {
-		parentBlock.children.push(blockCopy);
-	} else {
-		store.builderState.blocks.push(blockCopy);
-	}
-};
-
-const pasteStyle = () => {
-	props.block.updateStyles(store.copiedStyle?.style as BlockStyleObjects);
-};
-
-const contextMenuOptions: ContextMenuOption[] = [
-	{ label: "Copy Style", action: copyStyle },
-	{
-		label: "Paste Style",
-		action: pasteStyle,
-		condition: () => Boolean(store.copiedStyle && store.copiedStyle.blockId !== props.block.blockId),
-	},
-	{ label: "Save as Component", action: () => (showDialog.value = true) },
-	{ label: "Duplicate", action: duplicateBlock },
-];
 </script>
--- frontend/src/components/BlockLayers.vue
+++ frontend/src/components/BlockLayers.vue
@@ -7,59 +7,66 @@
 			item-key="blockId"
 			:disabled="blocks.length && blocks[0].isRoot()">
 			<template #item="{ element }">
-				<div
-					class="cursor-pointer rounded border-[1px] bg-white pl-2 pr-[2px] text-sm text-gray-600 dark:bg-zinc-900"
-					:class="{
-						'border-transparent text-gray-700 dark:text-gray-500':
-							!element.isSelected() && !element.isHovered(),
-						'border-blue-200 text-gray-600 dark:border-blue-800 dark:text-gray-500':
-							element.isHovered() && !element.isSelected(),
-						'border-blue-400 text-gray-900 dark:border-blue-600 dark:text-gray-200': element.isSelected(),
-					}"
-					@click.stop="store.selectBlock(element, $event)"
-					@mouseover.stop="store.hoveredBlock = element.blockId"
-					@mouseleave.stop="store.hoveredBlock = null">
-					<span class="my-[6px] flex items-center font-medium">
-						<FeatherIcon
-							:name="!element.collapsed ? 'chevron-down' : 'chevron-right'"
-							class="mr-1 h-3 w-3"
-							v-if="element.children && element.children.length && !element.isRoot() && !element.isComponent"
-							@click.stop="element.collapsed = !element.collapsed" />
-						<FeatherIcon :name="element.getIcon()" class="mr-1 h-3 w-3" v-if="!element.isComponent" />
-						<svg
-							class="mr-1 h-3 w-3"
-							xmlns="http://www.w3.org/2000/svg"
-							width="16"
-							height="16"
-							viewBox="0 0 24 24"
-							v-if="element.isComponent">
-							<g
-								fill="none"
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="2">
-								<rect width="7" height="9" x="3" y="3" rx="1" />
-								<rect width="7" height="5" x="14" y="3" rx="1" />
-								<rect width="7" height="9" x="14" y="12" rx="1" />
-								<rect width="7" height="5" x="3" y="16" rx="1" />
-							</g>
-						</svg>
-						<span
-							class="min-h-[1em] min-w-[2em] truncate"
-							:contenteditable="element.editable"
-							:title="element.blockId"
-							@dblclick="element.editable = true"
-							@keyup.enter.stop.prevent="element.editable = false"
-							@blur="setBlockName($event, element)">
-							{{ element.getBlockDescription() }}
-						</span>
-					</span>
-					<div
-						v-show="!element.collapsed"
-						v-if="!(element.isComponent || element.isText() || element.isImage())">
-						<BlockLayers :blocks="element.children" class="ml-1" />
-					</div>
+				<div>
+					<BlockContextMenu v-slot="{ onContextMenu }" :block="element" :editable="false">
+						<div
+							@contextmenu.prevent.stop="onContextMenu"
+							class="cursor-pointer rounded border-[1px] bg-white pl-2 pr-[2px] text-sm text-gray-600 dark:bg-zinc-900"
+							:class="{
+								'border-transparent text-gray-700 dark:text-gray-500':
+									!element.isSelected() && !element.isHovered(),
+								'border-blue-200 text-gray-600 dark:border-blue-800 dark:text-gray-500':
+									element.isHovered() && !element.isSelected(),
+								'border-blue-400 text-gray-900 dark:border-blue-600 dark:text-gray-200': element.isSelected(),
+							}"
+							@click.stop="store.selectBlock(element, $event)"
+							@mouseover.stop="store.hoveredBlock = element.blockId"
+							@mouseleave.stop="store.hoveredBlock = null">
+							<span class="my-[6px] flex items-center font-medium">
+								<FeatherIcon
+									:name="!element.collapsed ? 'chevron-down' : 'chevron-right'"
+									class="mr-1 h-3 w-3"
+									v-if="
+										element.children && element.children.length && !element.isRoot() && !element.isComponent
+									"
+									@click.stop="element.collapsed = !element.collapsed" />
+								<FeatherIcon :name="element.getIcon()" class="mr-1 h-3 w-3" v-if="!element.isComponent" />
+								<svg
+									class="mr-1 h-3 w-3"
+									xmlns="http://www.w3.org/2000/svg"
+									width="16"
+									height="16"
+									viewBox="0 0 24 24"
+									v-if="element.isComponent">
+									<g
+										fill="none"
+										stroke="currentColor"
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										stroke-width="2">
+										<rect width="7" height="9" x="3" y="3" rx="1" />
+										<rect width="7" height="5" x="14" y="3" rx="1" />
+										<rect width="7" height="9" x="14" y="12" rx="1" />
+										<rect width="7" height="5" x="3" y="16" rx="1" />
+									</g>
+								</svg>
+								<span
+									class="min-h-[1em] min-w-[2em] truncate"
+									:contenteditable="element.editable"
+									:title="element.blockId"
+									@dblclick="element.editable = true"
+									@keyup.enter.stop.prevent="element.editable = false"
+									@blur="setBlockName($event, element)">
+									{{ element.getBlockDescription() }}
+								</span>
+							</span>
+							<div
+								v-show="!element.collapsed"
+								v-if="!(element.isComponent || element.isText() || element.isImage())">
+								<BlockLayers :blocks="element.children" class="ml-1" />
+							</div>
+						</div>
+					</BlockContextMenu>
 				</div>
 			</template>
 		</draggable>
@@ -71,6 +78,7 @@ import { FeatherIcon } from "frappe-ui";
 import { PropType } from "vue";
 import draggable from "vuedraggable";
 import useStore from "../store";
+import BlockContextMenu from "./BlockContextMenu.vue";
 
 const store = useStore();
 defineProps({
