67cfe942e41d0a602d6f8284184285d393556a91
Suraj Shetty <surajshetty3416@gmail.com>
2023-08-04 23:36:19 +0530

    feat: Custom autocomplete

---
+++ frontend/src/components/Autocomplete.vue
@@ -0,0 +1,75 @@
+<template>
+	<div>
+		<Combobox :modelValue="value" @update:modelValue="(val) => emit('update:modelValue', val)" nullable>
+			<ComboboxInput
+				autocomplete="off"
+				@change="query = $event.target.value"
+				:displayValue="(option: Option) => (option ? option.label : '')"
+				:placeholder="placeholder"
+				class="flex h-7 w-full items-center justify-between gap-2 rounded bg-gray-100 px-2 py-1 text-sm text-gray-800 outline-none transition-colors hover:bg-gray-200 focus:ring-2 focus:ring-gray-400 dark:border-zinc-700 dark:bg-zinc-800 dark:text-zinc-200 dark:focus:bg-zinc-700" />
+			<ComboboxOptions
+				class="absolute right-0 z-50 max-h-[15rem] w-full max-w-[150px] overflow-y-auto rounded-lg bg-white px-1.5 py-1.5 shadow-2xl"
+				v-show="filteredValues.length">
+				<ComboboxOption v-if="query" :value="query" class="flex items-center"></ComboboxOption>
+				<ComboboxOption
+					v-slot="{ active, selected }"
+					v-for="option in filteredValues"
+					:key="option.value"
+					:value="option"
+					class="flex items-center">
+					<li
+						class="w-full select-none rounded px-2.5 py-1.5 text-xs"
+						:class="{
+							'bg-gray-100': active,
+							'bg-gray-300': selected,
+						}">
+						{{ option.label }}
+					</li>
+				</ComboboxOption>
+			</ComboboxOptions>
+		</Combobox>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { Combobox, ComboboxInput, ComboboxOption, ComboboxOptions } from "@headlessui/vue";
+import { ComputedRef, PropType, computed, ref } from "vue";
+const query = ref("");
+
+const filteredValues = computed(() =>
+	query.value === ""
+		? props.options
+		: props.options.filter((option) => {
+				return option.value.toLowerCase().includes(query.value.toLowerCase());
+		  })
+);
+
+type Option = {
+	label: string;
+	value: string;
+};
+const props = defineProps({
+	options: {
+		type: Array as PropType<Option[]>,
+		default: () => [],
+	},
+	modelValue: {},
+	placeholder: {
+		type: String,
+		default: "Search",
+	},
+});
+const value = computed(() => {
+	if (
+		props.modelValue instanceof String ||
+		typeof props.modelValue === "string" ||
+		props.modelValue instanceof Number ||
+		typeof props.modelValue === "number"
+	) {
+		return { label: props.modelValue, value: props.modelValue };
+	} else {
+		return props.modelValue;
+	}
+}) as ComputedRef<Option>;
+const emit = defineEmits(["update:modelValue", "update:query", "change"]);
+</script>
