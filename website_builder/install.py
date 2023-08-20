0cb427802328eb4d5bf8f608c3322b1a80de063d
Suraj Shetty <surajshetty3416@gmail.com>
2023-08-20 16:22:01 +0530

    fix: Keep all builder uploads in "Builder Uploads" folder

--- website_builder/install.py
+++ website_builder/install.py
@@ -1,5 +1,7 @@
 import frappe
+from frappe.core.api.file import create_new_folder
 
 
 def after_install():
 	frappe.utils.execute_in_shell("PLAYWRIGHT_BROWSERS_PATH=0 playwright install chromium")
+	create_new_folder("Builder Uploads", "Home")
