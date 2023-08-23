443da962c4afde70b360972cd6c8e743faaf3caf
Suraj Shetty <surajshetty3416@gmail.com>
2023-08-23 07:58:13 +0530

    feat: Interface to set placement of items in flexbox

--- frontend/src/components/BlockLayoutHandler.vue
+++ frontend/src/components/BlockLayoutHandler.vue
@@ -27,24 +27,31 @@
 				@update:modelValue="(val: string | number) => blockController.setStyle('flexDirection', val)"
 				class="w-fit self-end [&>div>button[aria-checked='false']]:dark:!bg-transparent [&>div>button[aria-checked='false']]:dark:!text-zinc-400 [&>div>button[aria-checked='true']]:dark:!bg-zinc-700 [&>div>button]:dark:!bg-zinc-700 [&>div>button]:dark:!text-zinc-100 [&>div]:dark:!bg-zinc-800"></TabButtons>
 		</div>
+		<div
+			class="items-top relative flex justify-between"
+			v-if="blockController.getStyle('display') === 'flex'">
+			<span class="inline-block text-[10px] font-medium uppercase text-gray-600 dark:text-zinc-400">
+				Placement
+			</span>
+			<div
+				class="grid h-16 w-16 grid-cols-3 items-center justify-items-center rounded-sm bg-gray-200 p-1 dark:bg-zinc-800">
+				<div
+					class="h-3 w-3 cursor-pointer rounded-sm bg-gray-300 hover:bg-gray-400 dark:bg-zinc-700 dark:hover:bg-zinc-600"
+					:class="{
+						'bg-gray-700 dark:!bg-zinc-500': activePlacement === option,
+					}"
+					v-for="option in placementOptions"
+					:key="option"
+					@click="setAlignment(option)"></div>
+			</div>
+		</div>
+
 		<InlineInput
 			v-if="blockController.getStyle('display') === 'flex'"
 			:modelValue="blockController.getStyle('justifyContent')"
 			type="select"
-			label="Placement"
+			label="Arrangement"
 			:options="[
-				{
-					label: blockController.getStyle('flexDirection') === 'row' ? 'Start' : 'Top',
-					value: 'flex-start',
-				},
-				{
-					label: blockController.getStyle('flexDirection') === 'row' ? 'Center' : 'Middle',
-					value: 'center',
-				},
-				{
-					label: blockController.getStyle('flexDirection') === 'row' ? 'End' : 'Bottom',
-					value: 'flex-end',
-				},
 				{ label: 'Space Between', value: 'space-between' },
 				{ label: 'Space Around', value: 'space-around' },
 				{ label: 'Space Evenly', value: 'space-evenly' },
@@ -52,47 +59,12 @@
 			@update:modelValue="(val: string | number) => blockController.setStyle('justifyContent', val)" />
 
 		<InlineInput
-			v-if="blockController.getStyle('display') === 'flex'"
-			label="Align"
-			:modelValue="blockController.getStyle('alignItems')"
-			type="select"
-			:options="[
-				{
-					label: blockController.getStyle('flexDirection') === 'column' ? 'Start' : 'Top',
-					value: 'flex-start',
-				},
-				{
-					label: blockController.getStyle('flexDirection') === 'column' ? 'Center' : 'Middle',
-					value: 'center',
-				},
-				{
-					label: blockController.getStyle('flexDirection') === 'column' ? 'End' : 'Bottom',
-					value: 'flex-end',
-				},
-			]"
-			@update:modelValue="(val: string | number) => blockController.setStyle('alignItems', val)" />
-		<InlineInput
 			label="Gap"
 			v-if="blockController.getStyle('display') === 'flex'"
 			type="text"
 			:modelValue="blockController.getStyle('gap')"
 			@update:modelValue="(val: string | number) => blockController.setStyle('gap', val)" />
 
-		<!-- flex basis -->
-		<!-- <InlineInput
-			v-if="blockController.getStyle('display') === 'flex'"
-			type="text"
-			:modelValue="blockController.getStyle('flexBasis')"
-			@update:modelValue="(val: string | number) => blockController.setStyle('flexBasis', val)">
-			Basis
-		</InlineInput>
-		<InlineInput
-			v-if="blockController.getStyle('display') === 'flex'"
-			type="text"
-			:modelValue="blockController.getStyle('flexGrow')"
-			@update:modelValue="(val: string | number) => blockController.setStyle('flexGrow', val)">
-			Grow
-		</InlineInput> -->
 		<div class="flex items-center justify-between" v-if="blockController.getStyle('display') === 'flex'">
 			<span class="inline-block text-[10px] font-medium uppercase text-gray-600 dark:text-zinc-400">
 				Wrap
@@ -111,6 +83,7 @@
 <script lang="ts" setup>
 import blockController from "@/utils/blockController";
 import { TabButtons } from "frappe-ui";
+import { computed } from "vue";
 import InlineInput from "./InlineInput.vue";
 
 const setLayout = (layout: string) => {
@@ -123,63 +96,139 @@ const setLayout = (layout: string) => {
 	}
 };
 
-const setAlignment = (alignment: string) => {
-	const parentBlock = blockController.getParentBlock();
+const placementOptions = [
+	"top-left",
+	"top-middle",
+	"top-right",
+	"middle-left",
+	"middle-middle",
+	"middle-right",
+	"bottom-left",
+	"bottom-middle",
+	"bottom-right",
+];
 
-	if (!parentBlock) {
-		return;
-	}
-	if (alignment === "top-right") {
-		parentBlock.setStyle("justifyContent", "flex-end");
-		parentBlock.setStyle("alignItems", "flex-start");
-		parentBlock.setStyle("display", "flex");
-	} else if (alignment === "top-middle") {
-		parentBlock.setStyle("justifyContent", "center");
-		parentBlock.setStyle("alignItems", "flex-start");
-		parentBlock.setStyle("display", "flex");
-	} else if (alignment === "top-left") {
-		parentBlock.setStyle("justifyContent", "flex-start");
-		parentBlock.setStyle("alignItems", "flex-start");
-		parentBlock.setStyle("display", "flex");
-	} else if (alignment === "middle-right") {
-		parentBlock.setStyle("alignItems", "center");
-		parentBlock.setStyle("justifyContent", "flex-end");
-		parentBlock.setStyle("display", "flex");
-	} else if (alignment === "middle-middle") {
-		parentBlock.setStyle("alignItems", "center");
-		parentBlock.setStyle("justifyContent", "center");
-		parentBlock.setStyle("display", "flex");
-	} else if (alignment === "middle-left") {
-		parentBlock.setStyle("alignItems", "center");
-		parentBlock.setStyle("justifyContent", "flex-start");
-		parentBlock.setStyle("display", "flex");
-	} else if (alignment === "bottom-right") {
-		parentBlock.setStyle("alignItems", "flex-end");
-		parentBlock.setStyle("justifyContent", "flex-end");
-		parentBlock.setStyle("display", "flex");
-	} else if (alignment === "bottom-middle") {
-		parentBlock.setStyle("alignItems", "flex-end");
-		parentBlock.setStyle("justifyContent", "center");
-		parentBlock.setStyle("display", "flex");
-	} else if (alignment === "bottom-left") {
-		parentBlock.setStyle("alignItems", "flex-end");
-		parentBlock.setStyle("justifyContent", "flex-start");
-		parentBlock.setStyle("display", "flex");
-	} else if (alignment === "left") {
-		parentBlock.setStyle("alignItems", "flex-start");
-		parentBlock.setStyle("display", "flex");
-	} else if (alignment === "right") {
-		parentBlock.setStyle("alignItems", "flex-end");
-		parentBlock.setStyle("display", "flex");
-	} else if (alignment === "center") {
-		parentBlock.setStyle("alignItems", "center");
-		parentBlock.setStyle("display", "flex");
-	} else if (alignment === "top") {
-		parentBlock.setStyle("justifyContent", "flex-start");
-		parentBlock.setStyle("display", "flex");
-	} else if (alignment === "bottom") {
-		parentBlock.setStyle("justifyContent", "flex-end");
-		parentBlock.setStyle("display", "flex");
+const activePlacement = computed(() => {
+	return placementOptions.filter((option) => {
+		const flexDirection = blockController.getStyle("flexDirection");
+		const justifyContent = blockController.getStyle("justifyContent");
+		const alignItems = blockController.getStyle("alignItems");
+		switch (option) {
+			case "top-left":
+				return justifyContent === "flex-start" && alignItems === "flex-start";
+			case "top-middle":
+				return (
+					(flexDirection === "row" && justifyContent === "center" && alignItems === "flex-start") ||
+					(flexDirection === "column" && justifyContent === "flex-start" && alignItems === "center")
+				);
+			case "top-right":
+				return (
+					(flexDirection === "row" && justifyContent === "flex-end" && alignItems === "flex-start") ||
+					(flexDirection === "column" && justifyContent === "flex-start" && alignItems === "flex-end")
+				);
+			case "middle-left":
+				return (
+					(flexDirection === "row" && justifyContent === "flex-start" && alignItems === "center") ||
+					(flexDirection === "column" && justifyContent === "center" && alignItems === "flex-start")
+				);
+			case "middle-middle":
+				return justifyContent === "center" && alignItems === "center";
+			case "middle-right":
+				return (
+					(flexDirection === "row" && justifyContent === "flex-end" && alignItems === "center") ||
+					(flexDirection === "column" && justifyContent === "center" && alignItems === "flex-end")
+				);
+			case "bottom-left":
+				return (
+					(flexDirection === "row" && justifyContent === "flex-start" && alignItems === "flex-end") ||
+					(flexDirection === "column" && justifyContent === "flex-end" && alignItems === "flex-start")
+				);
+			case "bottom-middle":
+				return (
+					(flexDirection === "row" && justifyContent === "center" && alignItems === "flex-end") ||
+					(flexDirection === "column" && justifyContent === "flex-end" && alignItems === "center")
+				);
+			case "bottom-right":
+				return justifyContent === "flex-end" && alignItems === "flex-end";
+		}
+	})[0];
+});
+
+const setAlignment = (alignment: string) => {
+	blockController.setStyle("display", "flex");
+	const flexDirection = blockController.getStyle("flexDirection");
+	switch (alignment) {
+		case "top-right":
+			if (flexDirection === "row") {
+				blockController.setStyle("justifyContent", "flex-end");
+				blockController.setStyle("alignItems", "flex-start");
+			} else {
+				blockController.setStyle("justifyContent", "flex-start");
+				blockController.setStyle("alignItems", "flex-end");
+			}
+			break;
+		case "top-middle":
+			if (flexDirection === "row") {
+				blockController.setStyle("justifyContent", "center");
+				blockController.setStyle("alignItems", "flex-start");
+			} else {
+				blockController.setStyle("justifyContent", "flex-start");
+				blockController.setStyle("alignItems", "center");
+			}
+			break;
+		case "top-left":
+			blockController.setStyle("justifyContent", "flex-start");
+			blockController.setStyle("alignItems", "flex-start");
+			break;
+		case "middle-right":
+			if (flexDirection === "row") {
+				blockController.setStyle("justifyContent", "flex-end");
+				blockController.setStyle("alignItems", "center");
+			} else {
+				blockController.setStyle("justifyContent", "center");
+				blockController.setStyle("alignItems", "flex-end");
+			}
+			break;
+		case "middle-middle":
+			blockController.setStyle("justifyContent", "center");
+			blockController.setStyle("alignItems", "center");
+			break;
+		case "middle-left":
+			if (flexDirection === "row") {
+				blockController.setStyle("justifyContent", "flex-start");
+				blockController.setStyle("alignItems", "center");
+			} else {
+				blockController.setStyle("justifyContent", "center");
+				blockController.setStyle("alignItems", "flex-start");
+			}
+			break;
+		case "bottom-right":
+			if (flexDirection === "row") {
+				blockController.setStyle("justifyContent", "flex-end");
+				blockController.setStyle("alignItems", "flex-end");
+			} else {
+				blockController.setStyle("justifyContent", "flex-end");
+				blockController.setStyle("alignItems", "flex-end");
+			}
+			break;
+		case "bottom-middle":
+			if (flexDirection === "row") {
+				blockController.setStyle("justifyContent", "center");
+				blockController.setStyle("alignItems", "flex-end");
+			} else {
+				blockController.setStyle("justifyContent", "flex-end");
+				blockController.setStyle("alignItems", "center");
+			}
+			break;
+		case "bottom-left":
+			if (flexDirection === "row") {
+				blockController.setStyle("justifyContent", "flex-start");
+				blockController.setStyle("alignItems", "flex-end");
+			} else {
+				blockController.setStyle("justifyContent", "flex-end");
+				blockController.setStyle("alignItems", "flex-start");
+			}
+			break;
 	}
 };
 </script>
