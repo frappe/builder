2696f2436d64f7e7d35a8ad0b983f12481c2acee
Suraj Shetty <surajshetty3416@gmail.com>
2023-08-26 20:31:53 +0530

    fix: Use component name as ID instead of hash

--- frontend/src/types/WebsiteBuilder/WebPageComponent.ts
+++ frontend/src/types/WebsiteBuilder/WebPageComponent.ts
@@ -1,18 +1,19 @@
-export interface WebPageComponent {
-	creation: string;
-	name: string;
-	modified: string;
-	owner: string;
-	modified_by: string;
-	docstatus: 0 | 1 | 2;
-	parent?: string;
-	parentfield?: string;
-	parenttype?: string;
-	idx?: number;
+
+export interface WebPageComponent{
+	creation: string
+	name: string
+	modified: string
+	owner: string
+	modified_by: string
+	docstatus: 0 | 1 | 2
+	parent?: string
+	parentfield?: string
+	parenttype?: string
+	idx?: number
 	/**	Component Name : Data	*/
-	component_name?: string;
+	component_name?: string
 	/**	Block : JSON	*/
-	block?: any;
+	block?: any
 	/**	For Web Page : Link - Web Page Beta	*/
-	for_web_page?: string;
+	for_web_page?: string
 }
