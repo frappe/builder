8749273b4f1492847f05d5094e2466c9545313fe
Suraj Shetty <surajshetty3416@gmail.com>
2023-11-26 22:06:32 +0530

    chore: Refactor patch

--- builder/builder/doctype/builder_page/patches/attach_client_script_to_builder_page.py
+++ builder/builder/doctype/builder_page/patches/attach_client_script_to_builder_page.py
@@ -1,38 +1,31 @@
-
 import frappe
 
 def execute():
 	"""Attach Client Script to Builder Page"""
 	for builder_page in frappe.db.sql("""select name, client_script, style from `tabBuilder Page`""", as_dict=True):
 		if builder_page.client_script:
-			if script_name := frappe.db.exists("Builder Client Script", {"script": builder_page.client_script}):
-				script_doc = frappe.get_doc("Builder Client Script", script_name)
-			else:
-				script_doc = frappe.get_doc({
-					"doctype": "Builder Client Script",
-					"script_type": "JavaScript",
-					"script": builder_page.client_script
-				}).insert(ignore_permissions=True)
-			frappe.get_doc({
-				"doctype": "Builder Page Client Script",
-				"parent": builder_page.name,
-				"builder_script": script_doc.name,
-				"parentfield": "client_scripts",
-				"parenttype": "Builder Page"
-			}).insert(ignore_permissions=True)
+			script_doc = get_or_create_builder_script("Builder Client Script", builder_page.client_script, "JavaScript")
+			create_builder_page_client_script(builder_page.name, script_doc.name)
 		elif builder_page.style:
-			if script_name := frappe.db.exists("Builder Client Script", {"script": builder_page.style}):
-				style_doc = frappe.get_doc("Builder Client Script", script_name)
-			else:
-				style_doc = frappe.get_doc({
-					"doctype": "Builder Client Script",
-					"script_type": "CSS",
-					"script": builder_page.style
-				}).insert(ignore_permissions=True)
-			frappe.get_doc({
-				"doctype": "Builder Page Client Script",
-				"parent": builder_page.name,
-				"builder_script": style_doc.name,
-				"parentfield": "client_scripts",
-				"parenttype": "Builder Page"
-			}).insert(ignore_permissions=True)
+			style_doc = get_or_create_builder_script("Builder Client Script", builder_page.style, "CSS")
+			create_builder_page_client_script(builder_page.name, style_doc.name)
+
+def get_or_create_builder_script(doctype, script, script_type):
+	script_name = frappe.db.exists(doctype, {"script": script})
+	if script_name:
+		return frappe.get_doc(doctype, script_name)
+	else:
+		return frappe.get_doc({
+			"doctype": doctype,
+			"script_type": script_type,
+			"script": script
+		}).insert(ignore_permissions=True)
+
+def create_builder_page_client_script(parent, builder_script, parentfield="client_scripts", parenttype="Builder Page"):
+	frappe.get_doc({
+		"doctype": "Builder Page Client Script",
+		"parent": parent,
+		"builder_script": builder_script,
+		"parentfield": parentfield,
+		"parenttype": parenttype
+	}).insert(ignore_permissions=True)
