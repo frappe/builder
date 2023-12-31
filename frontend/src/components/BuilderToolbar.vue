12b3373c728c35f994a814bb3f79d5148c02e8da
Suraj Shetty <surajshetty3416@gmail.com>
2023-12-31 12:38:05 +0530

    feat: In-app getting started video button
    - It links to a youtube playlist that will have how to videos

--- frontend/src/components/BuilderToolbar.vue
+++ frontend/src/components/BuilderToolbar.vue
@@ -27,6 +27,28 @@
 			</Tooltip>
 		</div>
 		<div class="absolute right-3 flex items-center">
+			<Dialog
+				style="z-index: 40"
+				:options="{
+					title: 'Get Started',
+					size: '4xl',
+				}"
+				v-model="showDialog">
+				<template #body-content>
+					<iframe
+						class="h-[60vh] w-full rounded-sm"
+						src="https://www.youtube-nocookie.com/embed/videoseries?si=8NvOFXFq6ntafauO&amp;controls=0&amp;list=PL3lFfCEoMxvwZsBfCgk6vLKstZx204xe3"
+						title="Frappe Builder - Get Started"
+						frameborder="0"
+						allowfullscreen></iframe>
+				</template>
+			</Dialog>
+			<button @click="showDialog = true">
+				<FeatherIcon
+					title="Toggle Theme"
+					name="info"
+					class="mr-4 h-4 w-4 cursor-pointer text-gray-600 dark:text-gray-400" />
+			</button>
 			<UseDark v-slot="{ isDark, toggleDark }">
 				<button @click="transitionTheme(toggleDark)">
 					<FeatherIcon
@@ -57,10 +79,10 @@
 import { UseDark } from "@vueuse/components";
 import { Dialog, Tooltip } from "frappe-ui";
 import { ref } from "vue";
+import useStore from "../store";
 
 const publishing = ref(false);
-
-import useStore from "../store";
+const showDialog = ref(false);
 
 declare global {
 	interface Document {
