dad0e582d0e32e7050c88d4fcd5fe8c7164aa3aa
Suraj Shetty <13928957+surajshetty3416@users.noreply.github.com>
2024-01-30 14:43:20 +0530

    ci: Init automated UI tests (#12)
    Co-authored-by: Frappe PR Bot <developers@frappe.io>

---
+++ frontend/cypress/support/commands.js
@@ -0,0 +1,38 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add("login", (email, password) => {
+	if (!email) {
+		email = Cypress.config("testUser") || "Administrator";
+	}
+	if (!password) {
+		password = Cypress.config("adminPassword");
+	}
+	cy.request({
+		url: "/api/method/login",
+		method: "POST",
+		body: { usr: email, pwd: password },
+	});
+});
