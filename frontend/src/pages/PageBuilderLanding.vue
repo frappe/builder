161565c6745abe4d7b7cc5bfe7d1dbe3380872d0
Suraj Shetty <surajshetty3416@gmail.com>
2024-02-26 09:52:37 +0530

    feat: Add option to create page with template

--- frontend/src/pages/PageBuilderLanding.vue
+++ frontend/src/pages/PageBuilderLanding.vue
@@ -65,109 +65,7 @@
 			<div v-else-if="!webPages.data?.length" class="flex flex-col items-center justify-center">
 				<p class="mt-4 text-center text-base text-gray-500">No matching pages found.</p>
 			</div>
-			<router-link
-				v-if="displayType === 'grid'"
-				v-for="page in webPages.data"
-				:key="page.page_name"
-				:to="{ name: 'builder', params: { pageId: page.page_name } }"
-				class="max-w-[250px] flex-grow basis-52">
-				<div
-					class="group relative mr-2 w-full overflow-hidden rounded-md shadow hover:cursor-pointer dark:border-zinc-700 dark:bg-zinc-900 dark:text-zinc-200">
-					<img
-						width="250"
-						height="140"
-						:src="page.preview"
-						onerror="this.src='/assets/builder/images/fallback.png'"
-						class="w-full overflow-hidden rounded-lg bg-gray-50 object-cover p-2 dark:bg-zinc-900" />
-					<div class="flex items-center justify-between border-t-[1px] px-3 dark:border-zinc-800">
-						<span class="inline-block max-w-[160px] py-2 text-sm text-gray-700 dark:text-zinc-200">
-							<div class="flex items-center gap-1">
-								<p class="truncate">
-									{{ page.page_title || page.page_name }}
-								</p>
-							</div>
-							<UseTimeAgo v-slot="{ timeAgo }" :time="page.modified">
-								<p class="mt-1 block text-xs text-gray-500">Edited {{ timeAgo }}</p>
-							</UseTimeAgo>
-						</span>
-						<Dropdown
-							:options="[
-								{ label: 'Duplicate', onClick: () => duplicatePage(page), icon: 'copy' },
-								{ label: 'View in Desk', onClick: () => store.openInDesk(page), icon: 'arrow-up-right' },
-								{ label: 'Delete', onClick: () => deletePage(page), icon: 'trash' },
-							]"
-							size="sm"
-							placement="right">
-							<template v-slot="{ open }">
-								<FeatherIcon
-									name="more-vertical"
-									class="h-4 w-4 text-gray-500 hover:text-gray-700"
-									@click="open"></FeatherIcon>
-							</template>
-						</Dropdown>
-					</div>
-				</div>
-			</router-link>
-			<router-link
-				v-for="page in webPages.data"
-				v-if="displayType === 'list'"
-				:key="page.page_name"
-				:to="{ name: 'builder', params: { pageId: page.page_name } }"
-				class="h-fit w-full flex-grow">
-				<div
-					class="group relative mr-2 flex w-full overflow-hidden rounded-md shadow hover:cursor-pointer dark:border-zinc-700 dark:bg-zinc-900 dark:text-zinc-200">
-					<img
-						width="250"
-						height="140"
-						:src="page.preview"
-						onerror="this.src='/assets/builder/images/fallback.png'"
-						class="block w-44 overflow-hidden rounded-lg bg-gray-50 object-cover p-2 dark:bg-zinc-900" />
-					<div class="flex flex-1 items-start justify-between border-t-[1px] p-3 px-3 dark:border-zinc-800">
-						<span class="flex h-full flex-col justify-between text-sm text-gray-700 dark:text-zinc-200">
-							<div>
-								<div class="flex items-center gap-1">
-									<p class="truncate">
-										{{ page.page_title || page.page_name }}
-									</p>
-								</div>
-								<div class="mt-2 flex items-center gap-1">
-									<FeatherIcon name="globe" class="h-3 w-3 text-gray-500 hover:text-gray-700"></FeatherIcon>
-									<p class="text-xs text-gray-600">
-										{{ page.route }}
-									</p>
-								</div>
-							</div>
-							<div class="flex items-baseline gap-1">
-								<p class="mt-1 block text-xs text-gray-500">Created By {{ page.owner }}</p>
-								·
-								<UseTimeAgo v-slot="{ timeAgo }" :time="page.modified">
-									<p class="mt-1 block text-xs text-gray-500">Edited {{ timeAgo }}</p>
-								</UseTimeAgo>
-							</div>
-						</span>
-						<div class="flex items-center gap-2">
-							<Badge theme="green" v-if="page.published" class="dark:bg-green-900 dark:text-green-400">
-								Published
-							</Badge>
-							<Dropdown
-								:options="[
-									{ label: 'Duplicate', onClick: () => duplicatePage(page), icon: 'copy' },
-									{ label: 'View in Desk', onClick: () => store.openInDesk(page), icon: 'arrow-up-right' },
-									{ label: 'Delete', onClick: () => deletePage(page), icon: 'trash' },
-								]"
-								size="sm"
-								placement="right">
-								<template v-slot="{ open }">
-									<FeatherIcon
-										name="more-vertical"
-										class="h-4 w-4 text-gray-500 hover:text-gray-700"
-										@click="open"></FeatherIcon>
-								</template>
-							</Dropdown>
-						</div>
-					</div>
-				</div>
-			</router-link>
+			<PagePreviewCard v-for="page in webPages.data" :page="page" :mode="displayType"></PagePreviewCard>
 		</div>
 		<Button
 			class="m-auto mt-12 w-fit text-sm dark:bg-zinc-900 dark:text-zinc-300"
@@ -184,10 +82,15 @@
 			}"
 			v-model="showDialog">
 			<template #body-content>
-				<div
-					class="flex h-12 w-48 cursor-pointer items-center rounded-sm bg-gray-50 p-4 align-middle dark:bg-zinc-900"
-					@click="() => loadPage(null)">
-					Blank
+				<div>
+					<div
+						class="flex h-12 w-48 cursor-pointer items-center rounded-sm bg-gray-50 p-4 align-middle dark:bg-zinc-900"
+						@click="() => loadPage(null)">
+						Blank
+					</div>
+					<div class="flex flex-wrap gap-6">
+						<TemplatePagePreview v-for="page in webPages.data" :page="page"></TemplatePagePreview>
+					</div>
 				</div>
 			</template>
 		</Dialog>
@@ -195,17 +98,16 @@
 </template>
 <script setup lang="ts">
 import CrossIcon from "@/components/Icons/Cross.vue";
+import PagePreviewCard from "@/components/PagePreviewCard.vue";
+import TemplatePagePreview from "@/components/TemplatePagePreview.vue";
 import { webPages } from "@/data/webPage";
 import router from "@/router";
 import useStore from "@/store";
-import { BuilderPage } from "@/types/Builder/BuilderPage";
-import { confirm } from "@/utils/helpers";
-import { UseTimeAgo } from "@vueuse/components";
 import { useStorage, watchDebounced } from "@vueuse/core";
-import { Badge, Dropdown, TabButtons } from "frappe-ui";
-import { onMounted, ref } from "vue";
+import { TabButtons } from "frappe-ui";
+import { Ref, onMounted, ref } from "vue";
 
-const displayType = useStorage("displayType", "grid");
+const displayType = useStorage("displayType", "grid") as Ref<"grid" | "list">;
 
 const store = useStore();
 const searchFilter = ref("");
@@ -238,20 +140,6 @@ watchDebounced(
 	},
 	{ debounce: 300 }
 );
-
-const deletePage = async (page: BuilderPage) => {
-	const confirmed = await confirm(`Are you sure you want to delete Page: ${page.page_name}?`);
-	if (confirmed) {
-		await webPages.delete.submit(page.name);
-	}
-};
-
-const duplicatePage = async (page: BuilderPage) => {
-	const pageCopy = { ...page };
-	pageCopy.page_name = `${page.page_name}-copy`;
-	pageCopy.page_title = `${page.page_title} Copy`;
-	await webPages.insert.submit(pageCopy);
-};
 
 const loadMore = () => {
 	webPages.next();
