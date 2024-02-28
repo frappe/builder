660321910343fc9e9bd775127ac8c587c72adc06
Reshma396 <reshmakunjuty@gmail.com>
2024-02-28 11:48:53 +0530

    feat: one compliance website using frappe builder

--- builder/hooks.py
+++ builder/hooks.py
@@ -196,3 +196,5 @@ website_path_resolver = "builder.builder.doctype.builder_page.builder_page.resol
 page_renderer = "builder.builder.doctype.builder_page.builder_page.BuilderPageRenderer"
 
 get_web_pages_with_dynamic_routes = "builder.builder.doctype.builder_page.builder_page.get_web_pages_with_dynamic_routes"
+
+fixtures = [{"dt": "Builder Page", "filters": [["page_title", "in", ["Home", "Pricing", "Document"]]]}]
