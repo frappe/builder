1048d3863f087c12fa52a73a443afce7ed6878bc
Suraj Shetty <surajshetty3416@gmail.com>
2024-04-01 14:10:57 +0530

    fix: Self load required font files

---
+++ frontend/src/components/Reset.vue
@@ -0,0 +1,44 @@
+<template>
+	<div></div>
+</template>
+<script setup></script>
+<style>
+@font-face {
+	font-family: "InterVar";
+	font-weight: 100 900;
+	font-display: swap;
+	font-style: normal;
+	src: url("/assets/builder/fonts/Inter/Inter.var.woff2?v=3.19") format("woff2-variations"),
+		url("/assets/builder/fonts/Inter/Inter.var.woff2?v=3.19") format("woff2");
+	src: url("/assets/builder/fonts/Inter/Inter.var.woff2?v=3.19") format("woff2") tech("variations");
+}
+
+@font-face {
+	font-family: "InterVar";
+	font-weight: 100 900;
+	font-display: swap;
+	font-style: italic;
+	src: url("/assets/builder/fonts/Inter/Inter-Italic.var.woff2?v=3.19") format("woff2-variations"),
+		url("/assets/builder/fonts/Inter/Inter-Italic.var.woff2?v=3.19") format("woff2");
+	src: url("/assets/builder/fonts/Inter/Inter-Italic.var.woff2?v=3.19") format("woff2") tech("variations");
+}
+@tailwind base;
+
+@layer base {
+	ul,
+	ol {
+		list-style: revert;
+		padding: revert;
+	}
+}
+
+p:empty:before {
+	content: "";
+	display: inline-block;
+}
+.__text_block__ a {
+	color: var(--link-color);
+	text-decoration: underline;
+	background-color: transparent;
+}
+</style>
