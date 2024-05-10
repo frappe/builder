a1fbf4ce3fd444e0149ea4dd799e7ee1b6180158
Suraj Shetty <surajshetty3416@gmail.com>
2024-05-10 08:13:41 +0530

    refactor: remove unused code

--- builder/install.py
+++ builder/install.py
@@ -3,5 +3,4 @@ from frappe.core.api.file import create_new_folder
 
 
 def after_install():
-	frappe.utils.execute_in_shell("PLAYWRIGHT_BROWSERS_PATH=0 playwright install chromium")
 	create_new_folder("Builder Uploads", "Home")
