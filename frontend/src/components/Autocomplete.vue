91eb8b341f6446f9eb35a3ffdf97981fb832733d
Suraj Shetty <surajshetty3416@gmail.com>
2024-06-27 19:09:43 +0530

    fix: Autocomplete font size

--- frontend/src/components/Autocomplete.vue
+++ frontend/src/components/Autocomplete.vue
@@ -29,7 +29,7 @@
 						}
 					"
 					:placeholder="!modelValue ? placeholder : null"
-					class="h-full w-full border-none bg-transparent p-0 text-xs focus:border-none focus:ring-0" />
+					class="h-full w-full border-none bg-transparent p-0 text-base focus:border-none focus:ring-0" />
 			</div>
 			<ComboboxOptions
 				class="absolute right-0 z-50 max-h-[15rem] w-full max-w-[150px] overflow-y-auto rounded-lg bg-white px-1.5 py-1.5 shadow-2xl"
