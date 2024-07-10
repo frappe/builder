4d2b5dfcc3a4ebd4e637e574967b0e26827cfabd
Suraj Shetty <surajshetty3416@gmail.com>
2024-07-10 21:04:45 +0530

    fix: Update exported script on change

--- builder/builder/doctype/builder_component/builder_component.py
+++ builder/builder/doctype/builder_component/builder_component.py
@@ -1,9 +1,13 @@
 # Copyright (c) 2023, asdf and contributors
 # For license information, please see license.txt
 
+import os
+
 import frappe
 from frappe.model.document import Document
+from frappe.modules.export_file import export_to_files
 from frappe.website.utils import clear_website_cache
+
 
 class BuilderComponent(Document):
 	def before_insert(self):
@@ -12,6 +16,7 @@ class BuilderComponent(Document):
 
 	def on_update(self):
 		self.queue_action("clear_page_cache")
+		self.update_exported_component()
 
 	def clear_page_cache(self):
 		pages = frappe.get_all("Builder Page", filters={"published": 1}, fields=["name"])
@@ -20,4 +25,14 @@ class BuilderComponent(Document):
 			if page_doc.is_component_used(self.component_id):
 				clear_website_cache(page_doc.route)
 
-
+	def update_exported_component(self):
+		if not frappe.conf.developer_mode:
+			return
+		component_path = os.path.join(
+			frappe.get_app_path("builder"), "builder", "builder_component", self.name
+		)
+		if os.path.exists(component_path):
+			export_to_files(
+				record_list=[["Builder Component", self.name, "builder_component"]],
+				record_module="builder",
+			)
