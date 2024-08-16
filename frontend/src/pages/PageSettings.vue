819645547c13b32a4c398763aeb5b1f3e59e4d19
Suraj Shetty <surajshetty3416@gmail.com>
2024-08-16 16:17:17 +0530

    feat: Init settings

--- frontend/src/pages/PageSettings.vue
+++ frontend/src/pages/PageSettings.vue
@@ -1,12 +1,20 @@
 <template>
-	<div
-		class="toolbar flex h-14 items-center justify-center bg-white p-2 shadow-sm dark:border-b-[1px] dark:border-gray-800 dark:bg-zinc-900">
-		<div class="absolute left-3 flex items-center gap-3">
-			<!-- go back -->
-			<FeatherIcon
-				name="chevron-left"
-				class="h-6 w-6 cursor-pointer !text-gray-700 dark:!text-gray-200"
-				@click="$router.back()"></FeatherIcon>
+	<div class="h-screen w-full overflow-hidden bg-gray-200">
+		<div
+			class="toolbar relative z-10 flex h-14 items-center justify-center bg-white p-2 shadow-sm dark:border-b-[1px] dark:border-gray-800 dark:bg-zinc-900">
+			<div class="absolute left-3 flex items-center gap-3">
+				<!-- go back -->
+				<FeatherIcon
+					name="chevron-left"
+					class="h-6 w-6 cursor-pointer !text-gray-700 dark:!text-gray-200"
+					@click="$router.back()"></FeatherIcon>
+			</div>
+		</div>
+		<div class="flex h-full flex-1">
+			<div class="h-full w-72 bg-white shadow-sm"></div>
+			<div class="m-auto mt-10 h-full w-2/4 max-w-4xl rounded bg-white p-5 shadow-sm">
+				<span class="text-xl font-semibold">Page settings</span>
+			</div>
 		</div>
 	</div>
 </template>
