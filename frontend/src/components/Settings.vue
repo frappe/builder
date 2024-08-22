6859cafee4b906e613da661924bff10cf39b7739
Suraj Shetty <surajshetty3416@gmail.com>
2024-08-22 12:38:13 +0530

    fix: Move setting section to separate page

--- frontend/src/components/Settings.vue
+++ frontend/src/components/Settings.vue
@@ -1,111 +1,92 @@
 <template>
 	<div class="flex h-[80vh] overflow-hidden">
-		<div class="flex w-48 flex-col gap-4 p-5">
-			<span class="text-xl font-semibold text-gray-800 dark:text-zinc-200">Settings</span>
-			<div class="flex flex-col gap-2">
-				<span class="text-base text-gray-700 dark:text-zinc-400">General</span>
-				<span class="text-base text-gray-700 dark:text-zinc-400">Meta</span>
-				<span class="text-base text-gray-700 dark:text-zinc-400">Robots</span>
-				<span class="text-base text-gray-700 dark:text-zinc-400">Analytics</span>
+		<div class="flex w-48 flex-col gap-5 bg-gray-50 p-4 px-3">
+			<span class="px-2 py-1 text-xl font-semibold text-gray-900 dark:text-zinc-200">Settings</span>
+			<div class="flex flex-col" v-for="(item, index) in settingsSidebarItems" :key="index">
+				<span class="mb-2 px-3 text-base font-semibold text-gray-900 dark:text-zinc-200">
+					{{ item.title }}
+				</span>
+				<a
+					@click="() => selectItem(link.value)"
+					class="cursor-pointer rounded p-2 px-3 text-base text-gray-800 dark:text-zinc-500"
+					:class="{
+						'bg-white text-gray-800 shadow-sm dark:bg-zinc-900 dark:!text-zinc-300':
+							selectedItem === link.value,
+					}"
+					v-for="link in item.items">
+					{{ link.label }}
+				</a>
 			</div>
 		</div>
 		<div class="flex h-full flex-1 flex-col gap-5 bg-white p-5 dark:bg-zinc-900">
 			<div class="flex justify-between">
-				<span class="text-xl font-semibold text-gray-800 dark:text-zinc-200">Page Settings</span>
+				<span class="text-xl font-semibold text-gray-800 dark:text-zinc-200">
+					{{ selectedItemDoc?.title }}
+				</span>
 				<Button icon="x" @click="$emit('close')"></Button>
 			</div>
-			<div class="flex h-full flex-col gap-5 overflow-y-auto">
-				<div class="flex w-full gap-4">
-					<div class="flex flex-1 flex-col gap-5">
-						<div class="flex gap-4">
-							<Input
-								type="text"
-								label="Page Title"
-								:modelValue="store.activePage?.page_title"
-								@update:modelValue="(val) => store.updateActivePage('page_title', val)" />
-							<Input
-								type="text"
-								label="Page Route"
-								:modelValue="store.activePage?.route"
-								@update:modelValue="(val) => store.updateActivePage('route', val)" />
-						</div>
-						<Input
-							type="textarea"
-							label="Page Description"
-							:modelValue="store.activePage?.meta_description"
-							@update:modelValue="(val) => store.updateActivePage('meta_description', val)" />
-						<div class="flex justify-between gap-5">
-							<div class="flex flex-1 gap-4">
-								<div class="flex flex-1 flex-col gap-2">
-									<InputLabel>Meta Image</InputLabel>
-									<ImageUploader
-										:image_url="store.activePage?.meta_image"
-										@upload="(url: string) => store.updateActivePage('meta_image', url)"
-										@remove="() => store.updateActivePage('meta_image', '')" />
-								</div>
-								<div class="flex flex-1 flex-col gap-2">
-									<InputLabel>Favicon</InputLabel>
-									<ImageUploader
-										image_type="image/ico"
-										:image_url="store.activePage?.favicon"
-										@upload="(url: string) => store.updateActivePage('favicon', url)"
-										@remove="() => store.updateActivePage('favicon', '')" />
-								</div>
-							</div>
-							<!-- <div class="flex flex-col gap-2">
-								<div class="flex flex-col gap-2">
-									<InputLabel>Social Preview</InputLabel>
-								</div>
-								<div class="flex flex-col gap-2">
-									<div class="flex items-center justify-between">
-										<div
-											class="flex h-fit flex-col gap-1 rounded-md border border-gray-100 p-4 dark:border-zinc-800">
-											<img
-												:src="store.activePage?.meta_image || store.activePage?.preview"
-												alt="Favicon Preview"
-												class="h-28 rounded" />
-											<span class="mt-2 text-base text-blue-600">{{ store.activePage?.page_title }}</span>
-											<span class="text-sm italic text-gray-600">
-												{{ store.activePage?.meta_description }}
-											</span>
-										</div>
-									</div>
-								</div>
-							</div> -->
-						</div>
-					</div>
-				</div>
-				<hr class="dark:border-zinc-800" />
-				<div class="flex flex-col gap-2">
-					<div class="flex items-center justify-between">
-						<div class="flex flex-col gap-2">
-							<span class="text-base dark:text-zinc-200">Unpublish</span>
-							<p class="text-sm text-gray-600">Unpublish your page</p>
-						</div>
-						<Tooltip
-							:test="
-								store.activePage?.published ? 'Unpublish this page' : 'This page is already unpublished'
-							">
-							<Button variant="subtle" theme="red" @click="store.unpublishPage()">Unpublish</Button>
-						</Tooltip>
-					</div>
-				</div>
-			</div>
+			<component :is="(selectedItemDoc as SidebarItem).component" />
 		</div>
 	</div>
 </template>
 <script setup lang="ts">
-import ImageUploader from "@/components/ImageUploader.vue";
-import Input from "@/components/Input.vue";
-import InputLabel from "@/components/InputLabel.vue";
 import useStore from "@/store";
-import { Tooltip } from "frappe-ui";
-import { onActivated } from "vue";
+import { computed, onActivated, ref } from "vue";
+import PageGeneral from "./Settings/PageGeneral.vue";
 // check route for page id
 import { useRoute } from "vue-router";
+import GlobalAnalytics from "./Settings/GlobalAnalytics.vue";
+import GlobalGeneral from "./Settings/GlobalGeneral.vue";
+import GlobalMeta from "./Settings/GlobalMeta.vue";
+import PageAnalytics from "./Settings/PageAnalytics.vue";
+import PageMeta from "./Settings/PageMeta.vue";
+import PageRobots from "./Settings/PageRobots.vue";
 const route = useRoute();
 const store = useStore();
 const emit = defineEmits(["close"]);
+
+const selectedItem = ref<string>("page_general");
+
+type SidebarItem = {
+	label: string;
+	value: string;
+	component: any;
+	title: string;
+};
+
+const selectedItemDoc = computed(() => {
+	for (const item of settingsSidebarItems) {
+		for (const link of item.items) {
+			if (link.value === selectedItem.value) {
+				return link;
+			}
+		}
+	}
+});
+
+const settingsSidebarItems = [
+	{
+		title: "Page",
+		items: [
+			{ label: "General", value: "page_general", component: PageGeneral, title: "General" },
+			{ label: "Meta", value: "page_meta", component: PageMeta, title: "Meta" },
+			{ label: "Robots", value: "page_robots", component: PageRobots, title: "Robots" },
+			{ label: "Analytics", value: "page_analytics", component: PageAnalytics, title: "Analytics" },
+		],
+	},
+	{
+		title: "Global",
+		items: [
+			{ label: "General", value: "global_general", component: GlobalGeneral, title: "General" },
+			{ label: "Meta", value: "global_meta", component: GlobalMeta, title: "Meta" },
+			{ label: "Analytics", value: "global_analytics", component: GlobalAnalytics, title: "Analytics" },
+		],
+	},
+];
+
+const selectItem = (value: string) => {
+	selectedItem.value = value;
+};
 
 onActivated(() => {
 	if (route.params.pageId === store.activePage?.name) return;
---
+++ frontend/src/components/Settings/PageGeneral.vue
@@ -0,0 +1,67 @@
+<template>
+	<div class="flex h-full flex-col items-center gap-5 overflow-y-auto">
+		<div class="flex w-full max-w-[500px] gap-4">
+			<div class="flex flex-1 flex-col gap-5">
+				<div class="flex gap-4">
+					<Input
+						type="text"
+						label="Page Title"
+						:modelValue="store.activePage?.page_title"
+						@update:modelValue="(val) => store.updateActivePage('page_title', val)" />
+					<Input
+						type="text"
+						label="Page Route"
+						:modelValue="store.activePage?.route"
+						@update:modelValue="(val) => store.updateActivePage('route', val)" />
+				</div>
+				<Input
+					type="textarea"
+					label="Page Description"
+					:modelValue="store.activePage?.meta_description"
+					:hideClearButton="true"
+					@update:modelValue="(val) => store.updateActivePage('meta_description', val)" />
+				<div class="flex justify-between gap-5">
+					<div class="flex flex-1 gap-4">
+						<div class="flex flex-1 flex-col gap-2">
+							<InputLabel>Meta Image</InputLabel>
+							<ImageUploader
+								:image_url="store.activePage?.meta_image"
+								@upload="(url: string) => store.updateActivePage('meta_image', url)"
+								@remove="() => store.updateActivePage('meta_image', '')" />
+						</div>
+						<div class="flex flex-1 flex-col gap-2">
+							<InputLabel>Favicon</InputLabel>
+							<ImageUploader
+								image_type="image/ico"
+								:image_url="store.activePage?.favicon"
+								@upload="(url: string) => store.updateActivePage('favicon', url)"
+								@remove="() => store.updateActivePage('favicon', '')" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<hr class="w-full max-w-[500px] border-gray-200 dark:border-zinc-800" />
+		<div class="flex w-full max-w-[500px] flex-col gap-2">
+			<div class="flex items-center justify-between">
+				<div class="flex flex-col gap-2">
+					<span class="text-base dark:text-zinc-200">Unpublish</span>
+					<p class="text-sm text-gray-600">Unpublish your page</p>
+				</div>
+				<Tooltip
+					:test="store.activePage?.published ? 'Unpublish this page' : 'This page is already unpublished'">
+					<Button variant="outline" theme="red" @click="store.unpublishPage()">Unpublish</Button>
+				</Tooltip>
+			</div>
+		</div>
+	</div>
+</template>
+<script setup lang="ts">
+import ImageUploader from "@/components/ImageUploader.vue";
+import Input from "@/components/Input.vue";
+import InputLabel from "@/components/InputLabel.vue";
+import useStore from "@/store";
+import { Tooltip } from "frappe-ui";
+// check route for page id
+const store = useStore();
+</script>
