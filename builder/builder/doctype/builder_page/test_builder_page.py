47d0a82661c55108076e5776f8ba6ecae2b2229c
Suraj Shetty <surajshetty3416@gmail.com>
2024-08-26 19:50:52 +0530

    test: Init Builder Page test

--- builder/builder/doctype/builder_page/test_builder_page.py
+++ builder/builder/doctype/builder_page/test_builder_page.py
@@ -1,9 +1,25 @@
 # Copyright (c) 2023, asdf and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
+from frappe.website.serve import get_response_content
 
 
 class TestBuilderPage(FrappeTestCase):
-	pass
+	def setUp(self):
+		self.page = frappe.get_doc(
+			{
+				"doctype": "Builder Page",
+				"name": "test-page",
+				"page_title": "Test Page",
+				"published": 1,
+				"route": "/test-page",
+				"blocks": [{"element": "body", "children": [{"element": "h1", "innerHTML": "Hello World!"}]}],
+			}
+		).insert(ignore_if_duplicate=True)
+		print(self.page.name)
+
+	def test_can_render(self):
+		content = get_response_content("/test-page")
+		self.assertTrue("Hello World!" in content)
