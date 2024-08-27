382ebe17ee489bd7133b55cebac1b4cd4990e489
Suraj Shetty <surajshetty3416@gmail.com>
2024-08-27 08:14:45 +0530

    test: Add test cases for publishing & unpublishing

--- builder/builder/doctype/builder_page/test_builder_page.py
+++ builder/builder/doctype/builder_page/test_builder_page.py
@@ -2,13 +2,15 @@
 # See license.txt
 
 import frappe
+from frappe.desk.form.load import getdoc
 from frappe.tests.utils import FrappeTestCase
 from frappe.website.serve import get_response_content
 
 
 class TestBuilderPage(FrappeTestCase):
-	def setUp(self):
-		self.page = frappe.get_doc(
+	@classmethod
+	def setUpClass(cls):
+		cls.page = frappe.get_doc(
 			{
 				"doctype": "Builder Page",
 				"name": "test-page",
@@ -18,8 +20,45 @@ class TestBuilderPage(FrappeTestCase):
 				"blocks": [{"element": "body", "children": [{"element": "h1", "innerHTML": "Hello World!"}]}],
 			}
 		).insert(ignore_if_duplicate=True)
-		print(self.page.name)
+		cls.page_with_dynamic_route = frappe.get_doc(
+			{
+				"doctype": "Builder Page",
+				"page_title": "Test Page Dynamic Route",
+				"published": 1,
+				"route": "/test-page-dynamic-route/<name>",
+				"dynamic_route": 1,
+				"blocks": [
+					{"element": "body", "children": [{"element": "h1", "innerHTML": "Dynamic Content!"}]}
+				],
+			}
+		).insert(ignore_if_duplicate=True)
 
 	def test_can_render(self):
 		content = get_response_content("/test-page")
 		self.assertTrue("Hello World!" in content)
+
+	def test_onload(self):
+		getdoc("Builder Page", self.page.name)
+		self.assertEqual(frappe.response.docs[0].get("__onload").get("builder_path"), "builder")
+
+	def test_dynamic_route(self):
+		from frappe.utils import get_html_for_route
+
+		content = get_html_for_route("/test-page-dynamic-route/123")
+		self.assertTrue("Dynamic Content!" in content)
+
+	def test_publish_unpublish(self):
+		self.page.unpublish()
+		from frappe.utils import get_html_for_route
+
+		content = get_html_for_route("/test-page")
+		self.assertTrue("The page you are looking for has gone missing" in content)
+
+		self.page.publish()
+		content = get_response_content("/test-page")
+		self.assertTrue("Hello World!" in content)
+
+	@classmethod
+	def tearDownClass(cls):
+		cls.page.delete()
+		cls.page_with_dynamic_route.delete()
