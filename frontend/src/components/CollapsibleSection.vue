b9475c642187fa764459637dce2c80d24fab71c2
Suraj Shetty <surajshetty3416@gmail.com>
2024-09-14 17:31:04 +0530

    fix: Properties panel section toggle UX

--- frontend/src/components/CollapsibleSection.vue
+++ frontend/src/components/CollapsibleSection.vue
@@ -17,6 +17,7 @@
 	</div>
 </template>
 <script lang="ts" setup>
+import { toValue } from "@vueuse/core";
 import { ref, watch } from "vue";
 
 const props = defineProps({
@@ -25,12 +26,11 @@ const props = defineProps({
 		required: true,
 	},
 	sectionCollapsed: {
-		type: [Boolean, Object],
+		type: Boolean,
 		default: false,
 	},
 });
 
-const propCollapsed = ref(props.sectionCollapsed);
 const collapsed = ref(false);
 
 const toggleCollapsed = () => {
@@ -38,9 +38,9 @@ const toggleCollapsed = () => {
 };
 
 watch(
-	() => propCollapsed.value,
-	(newVal) => {
-		collapsed.value = newVal as boolean;
+	() => props.sectionCollapsed,
+	() => {
+		collapsed.value = toValue(props.sectionCollapsed);
 	},
 	{ immediate: true },
 );
