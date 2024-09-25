ef157a2753364400cc60c669075073e9943f2ded
Suraj Shetty <surajshetty3416@gmail.com>
2024-09-25 14:01:15 +0530

    fix: Add patch to create icon folder

--- builder/builder/doctype/builder_page/patches/create_upload_folder_for_builder.py
+++ builder/builder/doctype/builder_page/patches/create_upload_folder_for_builder.py
@@ -1,7 +1,8 @@
-
 import frappe
 from frappe.core.api.file import create_new_folder
+
 
 def execute():
 	"""create upload folder for builder"""
 	create_new_folder("Builder Uploads", "Home")
+	create_new_folder("Fonts", "Home/Builder Uploads")
