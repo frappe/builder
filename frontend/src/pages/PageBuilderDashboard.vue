8fa645e9c73f0cbeee69f8c2c8229ec3fb98a433
Suraj Shetty <surajshetty3416@gmail.com>
2024-10-14 10:24:48 +0530

    fix(list): Outer border radius

--- frontend/src/pages/PageBuilderDashboard.vue
+++ frontend/src/pages/PageBuilderDashboard.vue
@@ -165,13 +165,13 @@
 					"
 					:to="{ name: 'builder', params: { pageId: page.page_name } }">
 					<div
-						class="group relative flex w-full cursor-pointer flex-col gap-2 rounded-xl bg-surface-white p-3 hover:bg-surface-gray-2">
+						class="group relative flex w-full cursor-pointer flex-col gap-2 rounded-2xl bg-surface-white p-3 hover:bg-surface-gray-2">
 						<img
 							width="250"
 							height="140"
 							:src="page.preview"
 							onerror="this.src='/assets/builder/images/fallback.png'"
-							class="w-full overflow-hidden rounded-lg object-cover shadow dark:border dark:border-outline-gray-1" />
+							class="w-full overflow-hidden rounded-md object-cover shadow dark:border dark:border-outline-gray-1" />
 						<div class="flex items-center justify-between border-outline-gray-2">
 							<span class="inline-block max-w-[160px]">
 								<div class="flex items-center gap-1">
@@ -213,7 +213,7 @@
 										icon="more-horizontal"
 										size="sm"
 										variant="subtle"
-										class="bg-surface-white !text-text-icons-gray-4 hover:!text-text-icons-gray-9"
+										class="bg-surface-white !text-text-icons-gray-5 hover:!text-text-icons-gray-9"
 										@click="open"></BuilderButton>
 								</template>
 							</Dropdown>
@@ -234,7 +234,7 @@
 					"
 					class="col-span-full h-fit w-full flex-grow">
 					<div
-						class="group relative flex w-full gap-3 overflow-hidden border-b-[1px] border-outline-gray-1 p-3 hover:cursor-pointer hover:rounded-xl hover:bg-surface-gray-1">
+						class="group relative flex w-full gap-3 overflow-hidden border-b-[1px] border-outline-gray-1 p-3 hover:cursor-pointer hover:rounded-2xl hover:bg-surface-gray-1">
 						<img
 							width="140"
 							height="82"
