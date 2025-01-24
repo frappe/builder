b8767b92e2200bfb0ab98313357d59d35eae4929
Suraj Shetty <surajshetty3416@gmail.com>
2025-01-24 12:24:37 +0530

    fix: Handle invalid dataURL or InvalidCharacterException

--- frontend/src/utils/helpers.ts
+++ frontend/src/utils/helpers.ts
@@ -391,15 +391,20 @@ async function uploadImage(file: File, silent = false) {
 }
 
 function dataURLtoFile(dataurl: string, filename: string) {
-	let arr = dataurl.split(","),
-		mime = arr[0].match(/:(.*?);/)?.[1],
-		bstr = atob(arr[1]),
-		n = bstr.length,
-		u8arr = new Uint8Array(n);
-	while (n--) {
-		u8arr[n] = bstr.charCodeAt(n);
+	try {
+		let arr = dataurl.split(","),
+			mime = arr[0].match(/:(.*?);/)?.[1],
+			bstr = atob(arr[1]),
+			n = bstr.length,
+			u8arr = new Uint8Array(n);
+		while (n--) {
+			u8arr[n] = bstr.charCodeAt(n);
+		}
+		return new File([u8arr], filename, { type: mime });
+	} catch (error) {
+		console.error(`Failed to convert dataURL ${dataurl} to file.`);
+		return null;
 	}
-	return new File([u8arr], filename, { type: mime });
 }
 
 declare global {
