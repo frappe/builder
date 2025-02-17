ece6c96cc703794578743b3efbf82c21f9cd0ed8
Suraj Shetty <surajshetty3416@gmail.com>
2025-02-17 05:41:03 +0530

    fix: Folder rename and delete options

--- frontend/src/components/DashboardSidebar.vue
+++ frontend/src/components/DashboardSidebar.vue
@@ -24,7 +24,7 @@
 					@click="showNewFolderDialog = true"></BuilderButton>
 			</div>
 			<span
-				class="flex cursor-pointer gap-2 rounded p-2 text-base text-ink-gray-6"
+				class="flex h-8 w-full cursor-pointer items-center justify-between gap-2 rounded p-2 py-1 text-base text-ink-gray-6"
 				v-for="project in builderProjectFolder.data"
 				:class="{
 					'bg-surface-modal text-ink-gray-8 shadow-sm dark:bg-surface-gray-2': isFolderActive(
@@ -32,26 +32,60 @@
 					),
 				}"
 				@click="setFolderActive(project.folder_name)">
-				<FolderIcon class="size-4"></FolderIcon>
-				{{ project.folder_name }}
+				<span class="flex flex-1 gap-1">
+					<FolderIcon class="size-4"></FolderIcon>
+					<EditableSpan
+						v-model="project.folder_name"
+						:editable="renamingFolder === project.folder_name"
+						@change="renameFolder"
+						class="w-full">
+						{{ project.folder_name }}
+					</EditableSpan>
+				</span>
+				<Dropdown
+					placement="right"
+					v-if="isFolderActive(project.folder_name)"
+					:options="[
+						{
+							label: 'Rename',
+							onClick: () => {
+								renamingFolder = project.folder_name;
+							},
+							icon: 'edit',
+						},
+						{
+							label: 'Delete Folder',
+							onClick: () => deleteFolder(project.folder_name),
+							icon: 'trash',
+						},
+					]">
+					<template v-slot="{ open }">
+						<BuilderButton icon="more-horizontal" size="sm" variant="ghost" @click="open"></BuilderButton>
+					</template>
+				</Dropdown>
 			</span>
 		</div>
 		<NewFolder v-model="showNewFolderDialog"></NewFolder>
 	</section>
 </template>
 <script lang="ts" setup>
+import BuilderButton from "@/components/Controls/BuilderButton.vue";
+import EditableSpan from "@/components/EditableSpan.vue";
 import FilesIcon from "@/components/Icons/Files.vue";
 import FolderIcon from "@/components/Icons/Folder.vue";
 import SettingsIcon from "@/components/Icons/SettingsGear.vue";
 import NewFolder from "@/components/Modals/NewFolder.vue";
 import builderProjectFolder from "@/data/builderProjectFolder";
 import useStore from "@/store";
+import { BuilderProjectFolder } from "@/types/Builder/BuilderProjectFolder";
+import { confirm } from "@/utils/helpers";
+import { createResource } from "frappe-ui";
+import Dropdown from "frappe-ui/src/components/Dropdown.vue";
 import { ref } from "vue";
 
 const store = useStore();
-
-const emit = defineEmits(["openSettings", "setActiveFolder"]);
-
+const renamingFolder = ref("");
+const emit = defineEmits(["openSettings"]);
 const showNewFolderDialog = ref(false);
 
 const isFolderActive = (folderName: string) => {
@@ -59,5 +93,43 @@ const isFolderActive = (folderName: string) => {
 };
 const setFolderActive = (folderName: string) => {
 	store.activeFolder = folderName;
+};
+
+const renameFolder = async (folderName: string) => {
+	if (!folderName) return;
+	await createResource({
+		url: "frappe.client.rename_doc",
+	}).submit({
+		doctype: "Builder Project Folder",
+		old_name: store.activeFolder,
+		new_name: folderName,
+	});
+	builderProjectFolder.data = builderProjectFolder.data.map((folder: BuilderProjectFolder) => {
+		if (folder.folder_name === store.activeFolder) {
+			folder.folder_name = folderName;
+		}
+		return folder;
+	});
+	renamingFolder.value = "";
+	setFolderActive(folderName);
+};
+
+const deleteFolder = async (folderName: string) => {
+	const confirmed = await confirm(
+		'Are you sure you want to delete this folder? All the pages under this folder will be visible under "All Pages"',
+	);
+	if (!confirmed) return;
+	await createResource({
+		url: "builder.api.delete_folder",
+		method: "POST",
+		params: {
+			folder_name: folderName,
+		},
+		auto: true,
+	});
+	builderProjectFolder.data = builderProjectFolder.data.filter(
+		(folder: BuilderProjectFolder) => folder.folder_name !== folderName,
+	);
+	setFolderActive("");
 };
 </script>
