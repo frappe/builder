9950ab5e70174a80dcc59604aae78d68b4ba3a15
Suraj Shetty <surajshetty3416@gmail.com>
2025-02-19 13:04:17 +0530

    feat: Init head script

--- frontend/src/components/Settings/PageCode.vue
+++ frontend/src/components/Settings/PageCode.vue
@@ -1,5 +1,25 @@
 <template>
-	<div class="no-scrollbar flex flex-col gap-5 overflow-auto">
+	<div class="no-scrollbar flex flex-col gap-6 overflow-auto">
+		<div>
+			<div class="">
+				<InputLabel>Load Library</InputLabel>
+				<div class="mb-2 flex">
+					<BuilderInput class="w-full"></BuilderInput>
+					<BuilderButton class="ml-2">Load</BuilderButton>
+				</div>
+			</div>
+			<div class="flex flex-col gap-2">
+				<div v-for="script in scripts" class="rounded bg-surface-gray-2 px-3 pr-0 shadow-sm">
+					<div class="flex w-full items-center justify-between gap-2">
+						<div class="flex items-center gap-2">
+							<span class="text-ink-gray-4">{{ script.type }}</span>
+							<a href="{{ script.script_url }}" class="text-sm hover:underline">{{ script.script_url }}</a>
+						</div>
+						<BuilderButton class="ml-2 text-sm">Remove</BuilderButton>
+					</div>
+				</div>
+			</div>
+		</div>
 		<CodeEditor
 			label="<head> code"
 			type="HTML"
@@ -12,8 +32,20 @@
 	</div>
 </template>
 <script setup lang="ts">
+import BuilderButton from "@/components/Controls/BuilderButton.vue";
 import CodeEditor from "@/components/Controls/CodeEditor.vue";
+import InputLabel from "@/components/Controls/InputLabel.vue";
 import useStore from "@/store";
-
 const store = useStore();
+
+const scripts = [
+	{
+		type: "js",
+		script_url: "https://cdn.jsdelivr.net/npm/vue@3.2.20/dist/vue.global.prod.js",
+	},
+	{
+		type: "css",
+		script_url: "https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css",
+	},
+];
 </script>
--- frontend/src/types/Builder/BuilderSettings.ts
+++ frontend/src/types/Builder/BuilderSettings.ts
@@ -1,27 +1,29 @@
-export interface BuilderSettings {
-	creation: string;
-	name: string;
-	modified: string;
-	owner: string;
-	modified_by: string;
-	docstatus: 0 | 1 | 2;
-	parent?: string;
-	parentfield?: string;
-	parenttype?: string;
-	idx?: number;
+
+export interface BuilderSettings{
+	creation: string
+	name: string
+	modified: string
+	owner: string
+	modified_by: string
+	docstatus: 0 | 1 | 2
+	parent?: string
+	parentfield?: string
+	parenttype?: string
+	idx?: number
+	/**	Head Script : Code - This will be appended at the end of the &lt;head&gt;	*/
+	head_script?: string
 	/**	Script : Code - Global script that will be loaded with every page built with Frappe Builder	*/
-	script?: string;
+	script?: string
 	/**	Script Public URL : Read Only	*/
-	script_public_url?: string;
+	script_public_url?: string
 	/**	Style : Code - Global style that will be loaded with every page built with Frappe Builder	*/
-	style?: string;
+	style?: string
 	/**	Style Public URL : Read Only	*/
-	style_public_url?: string;
+	style_public_url?: string
 	/**	Favicon : Attach Image - An icon file with .ico extension. Should be 16 x 16 px.<br>You can generate using <a href="https://favicon-generator.org" target="_blank">favicon-generator.org</a>	*/
-	favicon?: string;
+	favicon?: string
 	/**	Auto convert images to WebP : Check - All the images uploaded to Canvas will be auto converted to WebP for better page performance.	*/
-	auto_convert_images_to_webp?: 0 | 1;
+	auto_convert_images_to_webp?: 0 | 1
 	/**	Home Page : Data	*/
-	home_page?: string;
-	head_script?: string;
+	home_page?: string
 }
