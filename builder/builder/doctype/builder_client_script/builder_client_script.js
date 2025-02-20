e1812518ea1e33d0c8652ef84caab765ddc8b8ea
Suraj Shetty <surajshetty3416@gmail.com>
2025-02-20 10:59:59 +0530

    chore: Format files

--- builder/builder/doctype/builder_client_script/builder_client_script.js
+++ builder/builder/doctype/builder_client_script/builder_client_script.js
@@ -2,20 +2,20 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Builder Client Script", {
-	refresh(frm) {
-		frm.trigger("set_editor_options");
-	},
-	script_type(frm) {
-		frm.trigger("set_editor_options");
-	},
-	set_editor_options(frm) {
-		if (!frm.get_field("script").editor) {
-			return;
-		}
-		if (frm.doc.script_type == "CSS") {
-			frm.get_field("script").editor.session.setMode("ace/mode/css");
-		} else if (frm.doc.script_type == "JavaScript") {
-			frm.get_field("script").editor.session.setMode("ace/mode/javascript");
-		}
-	}
+  refresh(frm) {
+    frm.trigger("set_editor_options");
+  },
+  script_type(frm) {
+    frm.trigger("set_editor_options");
+  },
+  set_editor_options(frm) {
+    if (!frm.get_field("script").editor) {
+      return;
+    }
+    if (frm.doc.script_type == "CSS") {
+      frm.get_field("script").editor.session.setMode("ace/mode/css");
+    } else if (frm.doc.script_type == "JavaScript") {
+      frm.get_field("script").editor.session.setMode("ace/mode/javascript");
+    }
+  },
 });
