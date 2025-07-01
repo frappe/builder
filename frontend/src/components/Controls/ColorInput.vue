d0e64f28df43ede49ae58364bcff9ea27c3e245e
Suraj Shetty <surajshetty3416@gmail.com>
2025-07-01 20:40:38 +0530

    fix: Update ColorInput to accept modelValue and emit update:ModelValue

--- frontend/src/components/Controls/ColorInput.vue
+++ frontend/src/components/Controls/ColorInput.vue
@@ -1,34 +1,44 @@
 <template>
-	<ColorPicker :modelValue="value" @update:modelValue="(color) => emit('change', color)">
-		<template #target="{ togglePopover, isOpen }">
-			<div class="flex items-center justify-between">
-				<InputLabel>{{ label }}</InputLabel>
-				<div class="relative w-full">
-					<BuilderInput
-						type="text"
-						class="[&>div>input]:pl-8"
-						@focus="togglePopover"
-						:placeholder="placeholder"
-						:modelValue="value"
-						@update:modelValue="
-							(value: string | null) => {
-								value = getRGB(value);
-								emit('change', value);
-							}
-						">
-						<template #prefix>
-							<div
-								class="h-4 w-4 rounded shadow-sm"
-								@click="togglePopover"
-								:style="{
-									background: value ? value : `url(/assets/builder/images/color-circle.png) center / contain`,
-								}"></div>
-						</template>
-					</BuilderInput>
+	<div>
+		<ColorPicker
+			:modelValue="modelValue"
+			@update:modelValue="
+				(color) => {
+					emit('update:modelValue', color);
+				}
+			">
+			<template #target="{ togglePopover, isOpen }">
+				<div class="flex items-center justify-between">
+					<InputLabel v-if="label">{{ label }}</InputLabel>
+					<div class="relative w-full">
+						<BuilderInput
+							type="text"
+							class="[&>div>input]:pl-8"
+							@focus="togglePopover"
+							:placeholder="placeholder"
+							:modelValue="modelValue"
+							@update:modelValue="
+								(val: string | null) => {
+									const color = getRGB(val);
+									emit('update:modelValue', color);
+								}
+							">
+							<template #prefix>
+								<div
+									class="h-4 w-4 rounded shadow-sm"
+									@click="togglePopover"
+									:style="{
+										background: modelValue
+											? modelValue
+											: `url(/assets/builder/images/color-circle.png) center / contain`,
+									}"></div>
+							</template>
+						</BuilderInput>
+					</div>
 				</div>
-			</div>
-		</template>
-	</ColorPicker>
+			</template>
+		</ColorPicker>
+	</div>
 </template>
 <script setup lang="ts">
 import { getRGB } from "@/utils/helpers";
@@ -37,16 +47,15 @@ import InputLabel from "./InputLabel.vue";
 
 withDefaults(
 	defineProps<{
-		value?: HashString | null;
+		modelValue?: HashString | null;
 		label?: string;
 		placeholder?: string;
 	}>(),
 	{
-		value: null,
-		label: "",
+		modelValue: null,
 		placeholder: "Set Color",
 	},
 );
 
-const emit = defineEmits(["change"]);
+const emit = defineEmits(["update:modelValue"]);
 </script>
