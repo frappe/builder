5a00ce194ad739ccfa597e670a3b39c5ccf6a861
Suraj Shetty <surajshetty3416@gmail.com>
2025-07-01 13:11:07 +0530

    feat: Add own TabButtons to support default value

--- frontend/src/components/Controls/OptionToggle.vue
+++ frontend/src/components/Controls/OptionToggle.vue
@@ -5,18 +5,20 @@
 			class="w-full min-w-[150px] [&>div>button[aria-checked='true']]:dark:!bg-surface-gray-4 [&>div>button]:items-center [&>div]:h-7"
 			:buttons="options"
 			:modelValue="modelValue"
+			:defaultValue="defaultValue"
 			@update:modelValue="$emit('update:modelValue', $event)"></TabButtons>
 	</div>
 </template>
 <script setup lang="ts">
 import InputLabel from "@/components/Controls/InputLabel.vue";
-import { TabButtons } from "frappe-ui";
+import TabButtons from "@/components/Controls/TabButtons.vue";
 
 withDefaults(
 	defineProps<{
-		modelValue?: string | number | boolean | null;
+		modelValue?: string | number | boolean;
 		options?: { label: string; value: string | number; icon?: string; hideLabel?: boolean }[];
 		label?: string;
+		defaultValue?: string | number;
 	}>(),
 	{
 		options: () => [],
