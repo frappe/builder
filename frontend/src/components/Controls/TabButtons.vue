b6b17f508e59c8fd8b56a7210e3db93c05ee1711
stravo1@mac <stravoone@gmail.com>
2026-01-14 11:28:16 +0530

    feat(TabButtons): added tooltip if label is hidden

--- frontend/src/components/Controls/TabButtons.vue
+++ frontend/src/components/Controls/TabButtons.vue
@@ -7,23 +7,28 @@
 				:key="button.label"
 				:value="button.value ?? button.label"
 				v-slot="{ active, checked }">
-				<button
-					:class="[
-						active ? 'ring-outline-gray-2 focus-visible:ring' : '',
-						!modelValue && checked ? 'border border-dashed border-outline-gray-3' : '',
-						modelValue && checked ? 'bg-surface-white text-ink-gray-9 shadow' : 'text-ink-gray-7',
-						'flex flex-1 justify-center gap-2 whitespace-nowrap rounded-[7px] px-3 py-[5px] leading-none transition-colors focus:outline-none',
-					]">
-					<FeatherIcon
-						class="h-4 w-4"
-						v-if="button.icon"
-						:name="button.icon"
-						:label="button.label"
-						:aria-label="button.label" />
-					<RadioGroupLabel as="span" class="flex h-4 items-center" v-show="button.label && !button.hideLabel">
-						{{ button.label }}
-					</RadioGroupLabel>
-				</button>
+				<Tooltip :disabled="!button.hideLabel" :text="button.label" placement="top">
+					<button
+						:class="[
+							active ? 'ring-outline-gray-2 focus-visible:ring' : '',
+							!modelValue && checked ? 'border border-dashed border-outline-gray-3' : '',
+							modelValue && checked ? 'bg-surface-white text-ink-gray-9 shadow' : 'text-ink-gray-7',
+							'flex flex-1 justify-center gap-2 whitespace-nowrap rounded-[7px] px-3 py-[5px] leading-none transition-colors focus:outline-none',
+						]">
+						<FeatherIcon
+							class="h-4 w-4"
+							v-if="button.icon"
+							:name="button.icon"
+							:label="button.label"
+							:aria-label="button.label" />
+						<RadioGroupLabel
+							as="span"
+							class="flex h-4 items-center"
+							v-show="button.label && !button.hideLabel">
+							{{ button.label }}
+						</RadioGroupLabel>
+					</button>
+				</Tooltip>
 			</RadioGroupOption>
 		</div>
 	</RadioGroup>
@@ -31,7 +36,7 @@
 
 <script lang="ts" setup>
 import { RadioGroup, RadioGroupLabel, RadioGroupOption } from "@headlessui/vue";
-import { FeatherIcon } from "frappe-ui";
+import { FeatherIcon, Tooltip } from "frappe-ui";
 import { computed } from "vue";
 
 interface Button {
