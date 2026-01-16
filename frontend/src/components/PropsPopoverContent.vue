1ab976eeae3d650bd275663008fcefceeb5a3647
stravo1@mac <stravoone@gmail.com>
2026-01-16 16:02:38 +0530

    refactor(PropsPopoverContent): simplify toggle options and streamline state management

--- frontend/src/components/PropsPopoverContent.vue
+++ frontend/src/components/PropsPopoverContent.vue
@@ -6,16 +6,7 @@
 		<div v-if="showIsStandardInput" class="flex items-center justify-between">
 			<InputLabel>Is Standard</InputLabel>
 			<OptionToggle
-				:options="[
-					{
-						label: 'Yes',
-						value: 'true',
-					},
-					{
-						label: 'No',
-						value: 'false',
-					},
-				]"
+				:options="toggleOptions"
 				:model-value="isStandard"
 				@update:model-value="handleIsStandardChange" />
 		</div>
@@ -24,16 +15,8 @@
 			<Input
 				v-model="name"
 				placeholder="Enter prop name"
-				@update:model-value="
-					(val) => {
-						name = val;
-					}
-				"
-				@input="
-					(val) => {
-						name = val;
-					}
-				"></Input>
+				@update:model-value="(val) => (name = val)"
+				@input="(val) => (name = val)"></Input>
 		</div>
 		<div class="flex items-center justify-between">
 			<InputLabel>Type</InputLabel>
@@ -47,19 +30,11 @@
 		<div v-if="!isStandardBool" class="flex items-center justify-between">
 			<InputLabel v-model="value">Value</InputLabel>
 			<Input
-				v-if="selectedPropType == 'static'"
+				v-if="isStaticProp"
 				v-model="value"
 				placeholder="Enter prop name"
-				@update:model-value="
-					(val) => {
-						value = val;
-					}
-				"
-				@input="
-					(val) => {
-						value = val;
-					}
-				"></Input>
+				@update:model-value="(val) => (value = val)"
+				@input="(val) => (value = val)"></Input>
 			<Autocomplete
 				v-else
 				:make-fixed="true"
@@ -75,16 +50,7 @@
 		<div v-if="!isStandardBool" class="flex items-center justify-between">
 			<InputLabel>Pass Down</InputLabel>
 			<OptionToggle
-				:options="[
-					{
-						label: 'Yes',
-						value: 'true',
-					},
-					{
-						label: 'No',
-						value: 'false',
-					},
-				]"
+				:options="toggleOptions"
 				:model-value="isPassedDown"
 				@update:model-value="(val) => (isPassedDown = val)" />
 		</div>
@@ -92,16 +58,7 @@
 		<div v-if="false && !isStandardBool && isInFragmentMode" class="flex items-center justify-between">
 			<InputLabel>Is Editable</InputLabel>
 			<OptionToggle
-				:options="[
-					{
-						label: 'Yes',
-						value: 'true',
-					},
-					{
-						label: 'No',
-						value: 'false',
-					},
-				]"
+				:options="toggleOptions"
 				:model-value="isEditable"
 				@update:model-value="(val) => (isEditable = val)" />
 		</div>
@@ -110,26 +67,15 @@
 			<div v-if="false" class="flex items-center justify-between">
 				<InputLabel>Is Required</InputLabel>
 				<OptionToggle
-					:options="[
-						{
-							label: 'Yes',
-							value: 'true',
-						},
-						{
-							label: 'No',
-							value: 'false',
-						},
-					]"
+					:options="toggleOptions"
 					:model-value="String(standardPropOptions.isRequired)"
 					@update:model-value="(val) => (standardPropOptions.isRequired = val === 'true')" />
 			</div>
 			<component
-				:is="componentMapping[standardPropOptions.type as keyof typeof componentMapping]"
+				:is="standardPropTypeComponent"
 				:options="standardPropOptions.options || {}"
 				ref="optionsComponentRef"
-				@update:options="(options: any) => {
-					standardPropOptions.options = options;
-				}" />
+				@update:options="updateStandardPropOptions" />
 		</template>
 		<BuilderButton
 			:disabled="!name.trim().length"
@@ -179,36 +125,30 @@ const props = withDefaults(
 const canvasStore = useCanvasStore();
 const blockDataStore = useBlockDataStore();
 
+const STANDARD_PROP_TYPES = ["string", "number", "boolean", "select", "array", "object"] as const;
+const COMPONENT_MAPPING = {
+	string: StringOptions,
+	number: NumberOptions,
+	boolean: BooleanOptions,
+	select: SelectOptions,
+	array: ArrayOptions,
+	object: ObjectOptions,
+} as const;
+
+const TOGGLE_OPTIONS: { label: string; value: string }[] = [
+	{ label: "Yes", value: "true" },
+	{ label: "No", value: "false" },
+];
+
 const isStandard = ref(props.propDetails?.isStandard ? "true" : "false");
-const isStandardBool = computed(() => isStandard.value === "true");
-const isEditable = ref("false"); // TODO: required?
+const isEditable = ref("false");
 const isPassedDown = ref(props.propDetails?.isPassedDown ? "true" : "false");
 const name = ref(props.propName ?? "");
 const value = ref(props.propDetails?.value ?? "");
 const comesFrom = ref<BlockProps[string]["comesFrom"]>(props.propDetails?.comesFrom ?? null);
-const selectedNonStandardPropType = ref(
-	props.propDetails && !props.propDetails.isStandard ? !props.propDetails.isDynamic : "static",
-);
-const standardPropOptions = reactive<BlockPropsStandardOptions>(
-	props.propDetails && props.propDetails.isStandard
-		? {
-				...props.propDetails.standardOptions,
-				isRequired: Boolean(props.propDetails.standardOptions?.isRequired),
-				type: props.propDetails.standardOptions?.type || "string",
-		  }
-		: {
-				isRequired: false,
-				type: "string",
-				// defaultValue: "",
-				options: {},
-				dependencies: {},
-		  },
-);
-const standardPropDependencyMap = reactive<{ [key: string]: any }>(
-	props.propDetails && props.propDetails.isStandard
-		? props.propDetails.standardOptions?.dependencies || {}
-		: {},
-);
+const selectedNonStandardPropType = ref(getInitialNonStandardPropType());
+const standardPropOptions = reactive<BlockPropsStandardOptions>(getInitialStandardPropOptions());
+const standardPropDependencyMap = reactive<{ [key: string]: any }>(getInitialDependencies());
 const autoCompleteRef = ref<typeof Autocomplete | null>(null);
 const optionsComponentRef = ref<any>(null);
 
@@ -225,185 +165,191 @@ const emit = defineEmits({
 	}) => true,
 });
 
-const propTypes = computed(() => {
-	if (isStandardBool.value) {
-		return [
-			{
-				label: "String",
-				value: "string",
-			},
-			{
-				label: "Number",
-				value: "number",
-			},
-			{
-				label: "Boolean",
-				value: "boolean",
-			},
-			{
-				label: "Select",
-				value: "select",
-			},
-			{
-				label: "Array",
-				value: "array",
-			},
-			{
-				label: "Object",
-				value: "object",
-			},
-		];
-	} else {
-		return [
-			{
-				label: "Static",
-				value: "static",
-			},
-			{
-				label: "Dynamic",
-				value: "dynamic",
-			},
-		];
-	}
-});
+const isStandardBool = computed(() => isStandard.value === "true");
+const isInFragmentMode = computed(() => canvasStore.editingMode == "fragment");
+const isStaticProp = computed(() => selectedNonStandardPropType.value === "static");
 
 const showIsStandardInput = computed(() => {
-	return canvasStore.editingMode == "fragment" && !blockController.getFirstSelectedBlock()?.getParentBlock();
+	return isInFragmentMode.value && !blockController.getFirstSelectedBlock()?.getParentBlock();
 });
 
-const isInFragmentMode = computed(() => {
-	return canvasStore.editingMode == "fragment";
+const toggleOptions = computed(() => TOGGLE_OPTIONS);
+
+const propTypes = computed(() => {
+	return isStandardBool.value ? getStandardPropTypes() : getNonStandardPropTypes();
 });
 
 const selectedPropType = computed(() => {
-	if (isStandardBool.value) {
-		return standardPropOptions.type;
-	} else {
-		return selectedNonStandardPropType.value;
-	}
+	return isStandardBool.value ? standardPropOptions.type : selectedNonStandardPropType.value;
 });
 
-const STANDARD_PROP_TYPES = ["string", "number", "boolean", "select", "array", "object"];
+const standardPropTypeComponent = computed(() => {
+	return COMPONENT_MAPPING[standardPropOptions.type as keyof typeof COMPONENT_MAPPING];
+});
 
-const setPropType = (type: string) => {
-	if (isStandardBool.value && STANDARD_PROP_TYPES.includes(type)) {
-		standardPropOptions.type = type as "string" | "number" | "boolean" | "select" | "array" | "object";
-	} else {
-		selectedNonStandardPropType.value = type as "static" | "dynamic";
-	}
-	comesFrom.value = null;
-	value.value = "";
-};
+const currentBlock = computed(() => blockController.getFirstSelectedBlock());
 
 const pageDataArray = computed(() => {
-	const currentBlock = blockController.getFirstSelectedBlock();
-	if (currentBlock) {
-		return getDataArray(blockDataStore.getPageData(currentBlock.blockId) || {});
+	if (currentBlock.value) {
+		return getDataArray(blockDataStore.getPageData(currentBlock.value.blockId) || {});
 	}
 	return [];
 });
 
 const blockDataArray = computed(() => {
-	const currentBlock = blockController.getFirstSelectedBlock();
-	if (currentBlock) {
-		return getDataArray(blockDataStore.getBlockData(currentBlock.blockId, "passedDown") || {});
+	if (currentBlock.value) {
+		return getDataArray(blockDataStore.getBlockData(currentBlock.value.blockId, "passedDown") || {});
 	}
 	return [];
 });
 
-const getOptions = async (query: string) => {
-	let options: { label: string; value: string }[] = [];
-	pageDataArray.value.map((prop) => {
-		if (query.trim() == "" || prop.toLowerCase().includes(query.toLowerCase())) {
-			options.push({
-				label: prop,
-				value: `${prop}--dataScript`,
-			});
-		}
-	});
-	blockDataArray.value.map((prop) => {
-		if (query.trim() == "" || prop.toLowerCase().includes(query.toLowerCase())) {
-			options.push({
-				label: prop,
-				value: `${prop}--blockDataScript`,
-			});
-		}
-	});
-	console.log("Options: ", options, blockDataArray.value);
-	return options;
-};
+function getInitialNonStandardPropType() {
+	return props.propDetails && !props.propDetails.isStandard
+		? props.propDetails.isDynamic
+			? "dynamic"
+			: "static"
+		: "static";
+}
 
-const componentMapping = {
-	string: StringOptions,
-	number: NumberOptions,
-	boolean: BooleanOptions,
-	select: SelectOptions,
-	array: ArrayOptions,
-	object: ObjectOptions,
+function getInitialStandardPropOptions(): BlockPropsStandardOptions {
+	if (props.propDetails?.isStandard) {
+		return {
+			...props.propDetails.standardOptions,
+			isRequired: Boolean(props.propDetails.standardOptions?.isRequired),
+			type: props.propDetails.standardOptions?.type || "string",
+		};
+	}
+
+	return {
+		isRequired: false,
+		type: "string",
+		options: {},
+		dependencies: {},
+	};
+}
+
+function getInitialDependencies() {
+	return props.propDetails?.isStandard ? props.propDetails.standardOptions?.dependencies || {} : {};
+}
+
+function getStandardPropTypes() {
+	return [
+		{ label: "String", value: "string" },
+		{ label: "Number", value: "number" },
+		{ label: "Boolean", value: "boolean" },
+		{ label: "Select", value: "select" },
+		{ label: "Array", value: "array" },
+		{ label: "Object", value: "object" },
+	];
+}
+
+function getNonStandardPropTypes() {
+	return [
+		{ label: "Static", value: "static" },
+		{ label: "Dynamic", value: "dynamic" },
+	];
+}
+
+function isValidStandardPropType(
+	type: string,
+): type is "string" | "number" | "boolean" | "select" | "array" | "object" {
+	return STANDARD_PROP_TYPES.includes(type as any);
+}
+
+function setPropType(type: string) {
+	if (isStandardBool.value && isValidStandardPropType(type)) {
+		standardPropOptions.type = type;
+	} else {
+		selectedNonStandardPropType.value = type as "static" | "dynamic";
+	}
+	comesFrom.value = null;
+	value.value = "";
+}
+
+function filterDataOptions(dataArray: string[], query: string) {
+	return dataArray
+		.filter((prop) => query.trim() === "" || prop.toLowerCase().includes(query.toLowerCase()))
+		.map((prop) => ({ label: prop, value: prop }));
+}
+
+const getOptions = async (query: string) => {
+	const pageOptions = filterDataOptions(pageDataArray.value, query).map((opt) => ({
+		...opt,
+		value: `${opt.value}--dataScript`,
+	}));
+
+	const blockOptions = filterDataOptions(blockDataArray.value, query).map((opt) => ({
+		...opt,
+		value: `${opt.value}--blockDataScript`,
+	}));
+
+	return [...pageOptions, ...blockOptions];
 };
 
 const handleIsStandardChange = async (newVal: string) => {
 	isStandard.value = newVal;
 	await nextTick();
-	reset({
-		keepName: true,
-		keepIsStandard: true,
-		keepProps: false,
-		keepType: false,
-	});
+	resetState({ keepName: true, keepIsStandard: true, keepProps: false, keepType: false });
 };
 
 const handleTypeChange = async (newVal: string) => {
 	setPropType(newVal);
 	await nextTick();
-	reset({
-		keepName: true,
-		keepIsStandard: true,
-		keepProps: false,
-		keepType: true,
-	});
+	resetState({ keepName: true, keepIsStandard: true, keepProps: false, keepType: true });
 };
 
 const handleValueSelection = (option: string | null) => {
 	if (option) {
-		comesFrom.value = option.split("--").slice(-1)[0] as BlockProps[string]["comesFrom"];
-		value.value = option.split("--").slice(0, -1).join("--");
+		const parts = option.split("--");
+		comesFrom.value = parts[parts.length - 1] as BlockProps[string]["comesFrom"];
+		value.value = parts.slice(0, -1).join("--");
 	} else {
 		comesFrom.value = null;
 		value.value = "";
 	}
 };
 
-const reset = async (keepParams: {
+const updateStandardPropOptions = (options: any) => {
+	standardPropOptions.options = options;
+};
+
+interface ResetParams {
 	keepName: boolean;
 	keepIsStandard: boolean;
 	keepProps: boolean;
 	keepType: boolean;
-}) => {
-	const { keepName, keepIsStandard, keepProps, keepType } = keepParams;
-	const isStandardByDefault = isInFragmentMode.value && props.mode == "add";
+}
 
-	const details = keepProps ? props.propDetails ?? null : null;
+function getDefaultIsStandard(keepProps: boolean): string {
+	const isStandardByDefault = isInFragmentMode.value && props.mode === "add";
+	const propDetailsStandard = keepProps ? props.propDetails?.isStandard : undefined;
+	return propDetailsStandard !== undefined
+		? String(propDetailsStandard)
+		: isStandardByDefault
+		? "true"
+		: "false";
+}
 
-	if (!keepName) name.value = props.propName ?? "";
-	if (!keepIsStandard)
-		isStandard.value =
-			details?.isStandard || isStandardByDefault ? "true" : "false";
+function resetNonStandardState(keepProps: boolean, keepType: boolean) {
+	if (!keepType) {
+		const details = keepProps ? props.propDetails : null;
+		selectedNonStandardPropType.value = details?.isDynamic ? "dynamic" : "static";
+	}
+}
 
-	value.value = details?.value ?? "";
+function resetStandardState(keepProps: boolean, keepType: boolean) {
+	const details = keepProps ? props.propDetails : null;
 
 	if (details?.isStandard) {
-		const nextType = keepType ? standardPropOptions.type : details?.standardOptions?.type ?? "string";
+		const nextType = keepType ? standardPropOptions.type : details.standardOptions?.type ?? "string";
 
 		Object.assign(standardPropOptions, {
-			...details?.standardOptions,
-			isRequired: Boolean(details?.standardOptions?.isRequired),
+			...details.standardOptions,
+			isRequired: Boolean(details.standardOptions?.isRequired),
 			type: nextType,
 		});
 
-		const deps = details?.standardOptions?.dependencies ?? {};
-		Object.keys(standardPropDependencyMap).forEach((k) => delete standardPropDependencyMap[k]);
-		Object.assign(standardPropDependencyMap, deps);
+		updateDependencyMap(details.standardOptions?.dependencies || {});
 	} else if (isStandardBool.value) {
 		if (!keepType) {
 			standardPropOptions.type = "string";
@@ -412,61 +358,44 @@ const reset = async (keepParams: {
 		Object.assign(standardPropOptions, {
 			isRequired: false,
 			type: standardPropOptions.type,
-			// defaultValue: "",
 			options: {},
 			dependencies: {},
 		});
 
-		Object.assign(standardPropDependencyMap, {});
+		updateDependencyMap({});
+	}
+}
+
+function updateDependencyMap(deps: { [key: string]: any }) {
+	Object.keys(standardPropDependencyMap).forEach((k) => delete standardPropDependencyMap[k]);
+	Object.assign(standardPropDependencyMap, deps);
+}
+
+const resetState = async (params: ResetParams) => {
+	const { keepName, keepIsStandard, keepProps, keepType } = params;
+	const details = keepProps ? props.propDetails : null;
+
+	if (!keepName) {
+		name.value = props.propName ?? "";
+	}
+
+	if (!keepIsStandard) {
+		isStandard.value = getDefaultIsStandard(keepProps);
+	}
+
+	value.value = details?.value ?? "";
+
+	if (isStandardBool.value) {
+		resetStandardState(keepProps, keepType);
 	} else {
-		if (!keepType) {
-			selectedNonStandardPropType.value = details?.isDynamic ? "dynamic" : "static";
-		}
+		resetNonStandardState(keepProps, keepType);
 	}
 
 	optionsComponentRef.value?.reset(!!keepProps);
 };
 
-watch(
-	[() => props.propName, () => props.propDetails],
-	() =>
-		reset({
-			keepName: false,
-			keepIsStandard: false,
-			keepProps: true,
-			keepType: false,
-		}),
-	{
-		immediate: true,
-		deep: true,
-	},
-);
-
-watch(
-	selectedPropType,
-	() => {
-		autoCompleteRef.value?.refreshOptions();
-	},
-	{ immediate: true },
-);
-
-const save = async () => {
-	await nextTick();
-	console.log("Saving prop:", {
-		name: name.value,
-		isStandard: isStandardBool.value,
-		isDynamic: isStandardBool.value ? false : selectedNonStandardPropType.value === "dynamic",
-		isPassedDown: isPassedDown.value === "true",
-		comesFrom: comesFrom.value,
-		value: isStandardBool.value ? null : value.value,
-		standardOptions: isStandardBool.value
-			? {
-					...standardPropOptions,
-					dependencies: standardPropDependencyMap.value,
-			  }
-			: undefined,
-	});
-	const propValue: BlockProps[string] = {
+function buildPropValue(): BlockProps[string] {
+	return {
 		isStandard: isStandardBool.value,
 		isDynamic: isStandardBool.value ? false : selectedNonStandardPropType.value === "dynamic",
 		isPassedDown: isStandardBool.value || isPassedDown.value === "true",
@@ -479,11 +408,30 @@ const save = async () => {
 			  }
 			: undefined,
 	};
+}
+
+const save = async () => {
+	await nextTick();
+	const propValue = buildPropValue();
+
 	if (props.mode === "add") {
 		emit("add:prop", { name: name.value, value: propValue });
 	} else {
-		emit("update:prop", { oldPropName: props.propName!, newName: name.value, newValue: propValue });
+		emit("update:prop", {
+			oldPropName: props.propName!,
+			newName: name.value,
+			newValue: propValue,
+		});
 	}
 };
-defineExpose({ reset });
+
+watch(
+	[() => props.propName, () => props.propDetails],
+	() => resetState({ keepName: false, keepIsStandard: false, keepProps: true, keepType: false }),
+	{ immediate: true, deep: true },
+);
+
+watch(selectedPropType, () => autoCompleteRef.value?.refreshOptions(), { immediate: true });
+
+defineExpose({ reset: resetState });
 </script>
