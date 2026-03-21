e6d01ce3d7fc8256fc2c3af0e9a474d55dbd0933
Suraj Shetty <surajshetty3416@gmail.com>
2026-03-21 15:13:54 +0530

    fix: Stream while modifying a block and not for specific tasks

--- frontend/src/components/AIPageGeneratorModal.vue
+++ frontend/src/components/AIPageGeneratorModal.vue
@@ -426,10 +426,8 @@ const onModifyStream = (data: any) => {
 	if (data.task_type) remoteTaskType.value = data.task_type;
 	if (data.block_id) remoteBlockId.value = data.block_id;
 
-	// only if its not text rewrite or image replace, since those are handled differently
-	if (remoteTaskType.value === "rewrite_text" || remoteTaskType.value === "replace_image") {
+	if (!["rewrite_text", "replace_image"].includes(remoteTaskType.value ?? "")) {
 		throttledProcessModifyStreaming();
-		return;
 	}
 };
 
