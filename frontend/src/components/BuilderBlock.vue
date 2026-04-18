4b89f2c27b967c1df3f2c8337d8a532b58c57717
stravo1@mac <stravoone@gmail.com>
2026-04-18 22:58:51 +0530

    fix(BuilderBlock): Include breakpoint in client script execution functions

--- frontend/src/components/BuilderBlock.vue
+++ frontend/src/components/BuilderBlock.vue
@@ -423,8 +423,8 @@ watch(
 		const mode = builderSettings.doc?.execute_block_scripts_in_editor;
 		if (mode === "Don't Execute") return;
 
-		if (mode === "Restricted") executeBlockClientScriptRestricted(uidToUse, script, allResolvedProps.value);
-		else executeBlockClientScriptUnrestricted(uidToUse, script, allResolvedProps.value);
+		if (mode === "Restricted") executeBlockClientScriptRestricted(uidToUse, props.breakpoint, script, allResolvedProps.value);
+		else executeBlockClientScriptUnrestricted(uidToUse, props.breakpoint, script, allResolvedProps.value);
 	},
 	{ immediate: true },
 );
