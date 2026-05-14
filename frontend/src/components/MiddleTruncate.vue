402cab1bb8561c9156786113732750199ff1a49f
stravo1@mac <stravoone@gmail.com>
2026-05-14 20:57:23 +0530

    fix: handle endSplit text overflow with gradient masking

--- frontend/src/components/MiddleTruncate.vue
+++ frontend/src/components/MiddleTruncate.vue
@@ -1,12 +1,20 @@
 <template>
-	<component :is="as" class="flex w-full overflow-hidden whitespace-nowrap" :title="text">
-		<span class="min-w-4 truncate">{{ textParts.join(".") }}</span>
-		<span class="" v-if="textParts.length > 1">{{ textParts[textParts.length - 1] }}</span>
+	<component
+		:is="as"
+		ref="wrapper"
+		class="flex w-full overflow-hidden whitespace-nowrap"
+		:title="text"
+		:style="applyGradient">
+		<span class="min-w-8 truncate">{{ truncatedSplit }}</span>
+		<span v-if="textParts.length > 1">{{ endSplit }}</span>
 	</component>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, useTemplateRef, ref, watchEffect } from "vue";
+
+const wrapperRef: any = useTemplateRef("wrapper");
+const hasOverflow = ref(false);
 
 const props = withDefaults(
 	defineProps<{
@@ -18,7 +26,34 @@ const props = withDefaults(
 	},
 );
 
+watchEffect(() => {
+	if (!wrapperRef.value) return;
+
+	const checkOverflow = () => {
+		hasOverflow.value = wrapperRef.value!.scrollWidth > wrapperRef.value!.clientWidth;
+	};
+
+	checkOverflow();
+
+	const resizeObserver = new ResizeObserver(checkOverflow);
+	resizeObserver.observe(wrapperRef.value);
+
+	return () => resizeObserver.disconnect();
+});
+
 const textParts = computed(() => {
 	return props.text.split(".");
+});
+
+const truncatedSplit = computed(() => {
+	return textParts.value.slice(0, textParts.value.length - 1).join(".");
+});
+
+const endSplit = computed(() => {
+	return `.${textParts.value[textParts.value.length - 1]}`;
+});
+
+const applyGradient = computed(() => {
+	return hasOverflow.value ? "mask-image: linear-gradient(to right, black 90%, transparent)" : "";
 });
 </script>
