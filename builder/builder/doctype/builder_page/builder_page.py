6cdc37795aaf61e62a23a7734a75963235cd9b35
stravo1@mac <stravoone@gmail.com>
2026-06-07 20:35:28 +0530

    Revert "fix: remove unused overridden property"
    This reverts commit a35b930dcea1f3f8cdd1787581cc41912febde8e.

--- builder/builder/doctype/builder_page/builder_page.py
+++ builder/builder/doctype/builder_page/builder_page.py
@@ -634,6 +634,7 @@ def build_tag(
 	if effective_element == "body":
 		tag.append("{% include 'templates/generators/webpage_scripts.html' %}")
 
+
 	return tag
 
 
@@ -1398,6 +1399,9 @@ def extend_block(block, overridden_block):
 	if overridden_block.get("blockClientScript"):
 		block["blockClientScript"] = overridden_block.get("blockClientScript")
 		block["isBlockClientScriptOverridden"] = True
+
+	if overridden_block.get("blockDataScript"):
+		block["blockDataScript"] = overridden_block.get("blockDataScript")
 
 	dataKey = overridden_block.get("dataKey", {})
 	if not block.get("dataKey"):
