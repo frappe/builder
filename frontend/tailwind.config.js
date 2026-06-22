e588be4fe57d9dae66aa262a0590cdf6e35a2c4e
gaurahari-dasa <ganesh.hegde@hkm-group.org>
2026-06-22 11:24:26 +0530

    Remove unused path from Tailwind config
    The removed path refers to a node_modules folder in the parent directory which is clearly wrong as the node_modules folder in the current directory is where frappe-ui package will be found installed.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,7 +8,6 @@ export default {
 		"./index.html",
 		"./src/**/*.{vue,js,ts,jsx,tsx}",
 		"./node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}",
-		"../node_modules/frappe-ui/src/components/**/*.{vue,js,ts,jsx,tsx}",
 	],
 	plugins: [
 		plugin(function ({ matchUtilities, theme }) {
